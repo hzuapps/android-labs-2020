ff6bbfe54b254da14f08429b0ee81105889bb138
L1nzSec <1227812331@qq.com>
2020-09-21 11:16:01 +0800

    first test

---
+++ students/sec1814080911202/sec1814080911202Activity.java
@@ -0,0 +1,5 @@
+package edu.hzuapps;
+public class Sec1814080911202Activity {
+	// While android system start,it is already a main func .So android needn't a main func like java desktop development.
+	
+}
