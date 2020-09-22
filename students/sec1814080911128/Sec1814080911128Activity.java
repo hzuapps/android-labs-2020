ee004aa00087cf0dab984828e58d103c0a2b9762
Jadore147258369 <1040898286@qq.com>
2020-09-22 19:27:58 +0800

    #1 #63 第一次实验

--- students/sec1814080911128/Sec1814080911128Activity.java
+++ students/sec1814080911128/Sec1814080911128Activity.java
@@ -1,16 +1,5 @@
-{\rtf1\ansi\ansicpg936\cocoartf1504\cocoasubrtf830
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red27\green31\blue34;\red244\green246\blue249;}
-{\*\expandedcolortbl;;\cssrgb\c14118\c16078\c18039;\cssrgb\c96471\c97255\c98039;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww25100\viewh13360\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+package edu.hzuapps.androidlabs.sec1814080911128
 
-\f0\fs24 \cf0 package 
-\f1 \cf2 \cb3 \expnd0\expndtw0\kerning0
-edu.hzuapps.androidlabs.sec1814080911128\
-\
-public class Sec1814080911128Activity \{\
-\
-\}\
-\
+public class Sec1814080911128Activity {
+
 }
