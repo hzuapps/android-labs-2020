f290cd523009565baea7b7bc4c94b7e84bc19118
Xterisk <liffey.tech@outlook.com>
2020-09-22 15:19:22 +0800

    #1 #11 第1次实验

---
+++ students/net1814080903211/edu.hzuapps.androidlabs.net1814080903211.Net1814080903211Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.net1814080903211;
+
+public class Net1814080903211Activity {
+    static public void main(String[] args) {
+        System.out.println("Initialization.");
+    }
+}
