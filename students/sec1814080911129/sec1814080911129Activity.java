2cb35ad55b7466483f75e8a8db37c8c04f597379
wolkenthrhrut7 <wolkenthrut@Oultook.com>
2020-09-23 22:07:29 +0800

    Update sec1814080911129Activity.java

--- students/sec1814080911129/sec1814080911129Activity.java
+++ students/sec1814080911129/sec1814080911129Activity.java
@@ -1,5 +1,8 @@
-package edu.hzuapps.androidlabs.sec1814080911129
+package edu.hzuapps.androidlabs.sec1814080911129;
 
 public class Sec1814080911129Activity {
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+	}
 	
 }
