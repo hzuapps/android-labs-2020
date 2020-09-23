e881e21b22a569a32fe6a30832d327c284421b9f
Lcnc <1543032744@qq.com>
2020-09-23 13:41:00 +0800

    #1 #126 第1次实验

---
+++ students/net1814080903229/Net1814080903229Activity.java
@@ -0,0 +1,10 @@
+package subject1;
+
+public class Net1814080903229Activity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("这是我的实验一");
+	}
+
+}
