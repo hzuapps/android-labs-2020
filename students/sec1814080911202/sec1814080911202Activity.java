c6732edadc276798addfd2740f1bb809452932c6
L1nzSec <1227812331@qq.com>
2020-09-23 16:37:22 +0800

    #1 #34 第一次实验

--- students/sec1814080911202/sec1814080911202Activity.java
+++ students/sec1814080911202/sec1814080911202Activity.java
@@ -1,4 +1,4 @@
-package edu.hzuapps;
+package edu.hzuapps.androidlabs.sec1814080911202;
 public class Sec1814080911202Activity {
 	// While android system start,it is already a main func .So android needn't a main func like java desktop development.
 	
