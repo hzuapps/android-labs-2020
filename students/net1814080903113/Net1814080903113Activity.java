f20acac93c89a53e5b22a74a80249e450e59177d
Miraiiiii <974773303@qq.com>
2020-09-24 22:13:52 +0900

    #1 #158 第一次实验

---
+++ students/net1814080903113/Net1814080903113Activity.java
@@ -0,0 +1,6 @@
+package edu.hzuapps.androidlabs.net1814080903113;
+public class Net1814080903113Activity{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
