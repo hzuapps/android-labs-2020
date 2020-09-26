b59a30fceb3c2ee6bbf2155c96146b756ffe5361
lituquan <1242441055@qq.com>
2020-09-26 17:11:45 +0800

    #1 #163 第一次试è试验

---
+++ students/net1814080903131/Net1814080903131Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.net1814080903131;
+
+public class Net1814080903131Activity {
+    public static void main(String[] args) {
+        System.out.println("holle world.");
+    }
+}
