d766c867b6522a5ec23a00ac2b5c71849ebee65b
unknown <1142455296@qq.com>
2020-09-27 21:34:42 +0800

    #1 #272 第1次实验

---
+++ students/net1814080903340/Net1814080903340Activity.java
@@ -0,0 +1,6 @@
+package edu.hzuapps.androidlabs.net1814080903340;
+public class Net1814080903340Activity {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
