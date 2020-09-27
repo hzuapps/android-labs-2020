032d4873acde8bd0debf00982ee50e616b780b37
ljjljj <1657084374@qq.com>
2020-09-27 22:24:02 +0800

    #1 #282 第一次实验

---
+++ students/sec1814080911224/edu/hzuapps/androidlabs/sec1814080911224/Sec1814080911224Activity.java
@@ -0,0 +1,8 @@
+package edu.hzuapps.androidlabs.sec1814080911224;
+
+public class Sec1814080911224Activity {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
