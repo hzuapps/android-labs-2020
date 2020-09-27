cbc616e1b2cb7c5094691fff251cc6484ca3e219
jaydjxing <1693502585@qq.com>
2020-09-27 14:49:31 +0800

    删除不必要的文件

--- students/com1814080903317/Com1814080903317Activity.java
+++
@@ -1,5 +0,0 @@
-package com.example.students.com1814080903317;
-public class Com1814080903317Activity extends Activity{
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-}
--- students/net1814080903317/edu.hzuapps.androidlabs.net1814080903317.Net1814080903317Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.net1814080903317.Net1814080903317Activity;
-
-
-public class edu.hzuapps.androidlabs.net1814080903317.Net1814080903317Activity{
-
-
-
-}
