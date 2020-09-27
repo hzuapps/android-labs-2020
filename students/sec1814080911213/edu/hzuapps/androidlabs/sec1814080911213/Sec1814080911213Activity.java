4e69fe1fa67c007ab62ff1b92ea760266c6155ad
uzi0000 <907955651@qq.com>
2020-09-27 22:35:50 +0800

    #1 #283 第1次实验

---
+++ students/sec1814080911213/edu/hzuapps/androidlabs/sec1814080911213/Sec1814080911213Activity.java
@@ -0,0 +1,8 @@
+package edu.hzuapps.androidlabs.sec1814080911213;
+
+public class Sec1814080911213Activity {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
