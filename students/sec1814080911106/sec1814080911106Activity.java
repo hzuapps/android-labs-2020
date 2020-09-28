72b44171502373f56f460ae3194742ec0c13f1ee
18cs06 <1449767333@qq.com.com>
2020-09-28 11:24:40 +0800

    #1 #50 第1次实验

---
+++ students/sec1814080911106/sec1814080911106Activity.java
@@ -0,0 +1,8 @@
+package edu.hzuapps.androidlabs.sec1814080911106;
+
+public class sec1814080911106Activity {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
