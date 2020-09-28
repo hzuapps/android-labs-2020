9b50a5c101b4e0cea8b5ce6931c5b22bf99bb2df
18cs06 <71617993+18cs06@users.noreply.github.com>
2020-09-28 23:50:20 +0800

    Delete sec1814080911106Activity.java

--- students/sec1814080911106/sec1814080911106Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.sec1814080911106;
-
-public class Sec1814080911106Activity {
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
