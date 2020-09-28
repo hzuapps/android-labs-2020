70b4ebce146271c9150e7860903596dcc138638e
zhongzhitao <liuhaihuachiji@163.com>
2020-09-28 08:17:13 +0800

    1 #312 第1次实验

---
+++ students/net1814080903222/Net1814080903211Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.net1814080903222;
+
+public class Net1814080903222Activity.java {
+	static public void Main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
