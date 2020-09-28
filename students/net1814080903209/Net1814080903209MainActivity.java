42c23a5c712c81a8dfcf97dab7bfa323d43f1ad3
itgopan <itgopan@foxmail.com>
2020-09-28 12:48:28 +0800

    #2 #21 第2次实验

---
+++ students/net1814080903209/Net1814080903209MainActivity.java
@@ -0,0 +1,14 @@
+package com.gopan.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Net1814080903209MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
