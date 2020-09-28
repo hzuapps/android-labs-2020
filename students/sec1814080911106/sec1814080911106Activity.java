beba343b810c1d831d348e656dea106d6c6a6f1e
18cs06 <71617993+18cs06@users.noreply.github.com>
2020-09-28 23:47:13 +0800

    Update sec1814080911106Activity.java

--- students/sec1814080911106/sec1814080911106Activity.java
+++ students/sec1814080911106/sec1814080911106Activity.java
@@ -1,6 +1,6 @@
 package edu.hzuapps.androidlabs.sec1814080911106;
 
-public class sec1814080911106Activity {
+public class Sec1814080911106Activity {
 
     public static void main(String[] args) {
         System.out.println("hello world");
