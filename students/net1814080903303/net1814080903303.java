5c9fdde910a56b5a36fa1608d95dfb00f6f2ac93
tiamo669 <crd19806630636>
2020-09-29 01:08:30 +0800

    #1 #59 第一次实验

--- students/net1814080903303/net1814080903303.java
+++
@@ -1,5 +0,0 @@
-public class Net1814080903303Activity { 
- static public void main(String[] args) { 
- System.out.println("holle world."); 
- } 
- } 
---
+++ students/net1814080903303/net1814080903303Activity .java
@@ -0,0 +1,5 @@
+public class net1814080903031Activity { 
+ static public void main(String[] args) { 
+ System.out.println("hello world."); 
+ } 
+ } 
