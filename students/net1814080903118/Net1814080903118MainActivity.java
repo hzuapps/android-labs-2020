962049c9e4c33c9b10b59bd215df3233e8633864
GBSTS <3350861305@qq.com>
2020-09-30 09:17:59 +0800

    实验二

---
+++ students/net1814080903118/Net1814080903118MainActivity.java
@@ -0,0 +1,22 @@
+package edu.hzuapps.androidlabs;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Net1814080903118MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
