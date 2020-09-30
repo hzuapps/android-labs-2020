aaae0d3ebaa2fb95bcc9208524520322282dd0d8
Chanlex <766698547@qq.com>
2020-09-30 13:37:29 +0800

    实验二

---
+++ students/net1814080903226/实验二/Net1814080903226MyViewActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Net1814080903226MyViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.net_1814080903226_myview_activity);
+    }
+}
