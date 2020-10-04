f7503d1644c8c304f9f57a3b3d1ac1e1ac45026b
lxp99 <1604191933@qq.com>
2020-10-05 05:06:52 +0800

    #1 #384 第一次实验

--- students/sec1814080911142/2/Sec1814080911142Activity.java
+++
@@ -1,7 +0,0 @@
-edu.hzuapps.androidlabs.sec1814080911142.Sec1814080911142Activity.java
-
-public class Sec1814080911142Activity{
-
-public static void main(String[ ] args){
-System.out.println("hello world")
-}
