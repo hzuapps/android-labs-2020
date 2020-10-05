2e3063a56413bb27b18e6a487220df2b4eb7a241
GBSTS <3350861305@qq.com>
2020-10-05 09:06:05 +0800

    第二次实验

---
+++ students/net1814080903118/Net1814080903118PayAckActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Net1814080903118PayAckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net_1814080903118_pay_ack);
+    }
+}
