9271a3b27ba0d9b68140b659b712cfe2446aa893
Wumuuu <995150275@qq.com>
2020-10-05 21:05:38 +0800

    创建第一个类

---
+++ students/net1814080903218/Net1814080903218Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.net1814080903218;
+
+public class Net1814080903218Activity {
+    static public void main(String[] args) {
+        System.out.println("holle world.");
+    }
+}
