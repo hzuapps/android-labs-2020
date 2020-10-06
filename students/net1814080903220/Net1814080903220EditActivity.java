6ed1941029bc4401553e1b0069274d796c1ec905
JieBro333 <JieBro333.com>
2020-10-06 16:28:46 +0800

    #2 #81 第2次实验

---
+++ students/net1814080903220/Net1814080903220EditActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.Net1814080903220;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Net1814080903220EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
