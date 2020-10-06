3885a611a22fe81e70ab04b33852702b02169220
522090231 <522090231@qq.com>
2020-10-06 20:50:52 +0800

    #2 #15 第2次实验

---
+++ students/net1814080903207/Net1814080903207WrongActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.Net1814080903207;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Net1814080903207WrongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrong);
+    }
+}
