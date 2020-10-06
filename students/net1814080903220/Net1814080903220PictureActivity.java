4622a6be8977746ee6a9d4641882ad4cee64a2ac
JieBro333 <JieBro333.com>
2020-10-06 16:32:29 +0800

    #2 #81 第2次实验

---
+++ students/net1814080903220/Net1814080903220PictureActivity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidlabs.Net1814080903220;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Net1814080903220PictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+    }
+}
