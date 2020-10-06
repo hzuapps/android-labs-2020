c850e048bd4f3c50de3f9ef694d147f548b98723
1814080911138 <1244908079@qq.com>
2020-10-06 08:58:34 +0800

    删除了这个文件

--- students/sec1814080911138/Sec1814080911138Activity.java
+++
@@ -1,7 +0,0 @@
-edu.hzuapps.androidlabs.sec1814080911138.Sec1814080911138Activity.java
-
-public class Sec1814080911138Activity{
-
-public static void main(String[ ] args){
-System.out.println("hello world")
-}
