0ac87b08f52c027af88927de8855910111f77ae8
lxp99 <1604191933@qq.com>
2020-10-07 01:08:04 +0800

    #1 #384 第1次实验

--- students/sec1814080911142/Sec1814080911142Activity.java
+++ students/sec1814080911142/Sec1814080911142Activity.java
@@ -1,8 +1,6 @@
-edu.hzuapps.androidlabs.sec1814080911142.Sec1814080911142Activity.java
-
+package edu.hzuapps.androidlabs.sec1814080911142;
 public class Sec1814080911142Activity{
-
-public static void main(String[ ] args){
-System.out.println("hello world")
-}
-}
+     public static void main(String[] args){
+          System.out.println("hello world");
+     }
+} 
