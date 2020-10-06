4b6658dc9b4420d40394929e396f1a7aff66af3e
1814080911138 <1244908079@qq.com>
2020-10-06 16:23:44 +0800

    #1 #405 第1次实验

--- students/sec1814080911138/Sec1814080911138Activity.java
+++ students/sec1814080911138/Sec1814080911138Activity.java
@@ -3,5 +3,5 @@ edu.hzuapps.androidlabs.sec1814080911138.Sec1814080911138Activity.java
 public class Sec1814080911138Activity{
 
 public static void main(String[ ] args){
-System.out.println("hello world")
+System.out.println("Hello World")
 }
