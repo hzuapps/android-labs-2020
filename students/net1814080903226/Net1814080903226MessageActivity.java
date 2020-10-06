e5c9471ac4430dd051e17c41f47e6a51cc4be890
Chanlex <766698547@qq.com>
2020-10-06 15:04:39 +0800

    实验二

---
+++ students/net1814080903226/Net1814080903226MessageActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Net1814080903226MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.net_1814080903226_message_activity);
+    }
+}
