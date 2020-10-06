e5be681f7d2a228da9b89ce53b258ce93625f8cb
unknown <you@ailcomgit config --global user.email 1282610556@qq.comgit config --global user.name tillsunrisegit add students/net1814080903202/Net1814080903202Activity.javagit commit -m #1>
2020-10-06 15:47:07 +0800

    #2 #71 第2次实验

--- students/net1814080903202/Net1814080903202Activity.java
+++ students/net1814080903202/Net1814080903202Activity.java
@@ -1,4 +1,46 @@
 package edu.hzuapps.androidlabs.net1814080903202;
 
-public class Net1814080903202Activity{
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903202Activity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903202);
+        Button btn1 = (Button)findViewById(R.id.button1);
+        Button btn2 = (Button)findViewById(R.id.button2);
+        Button btn3 = (Button)findViewById(R.id.button3);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final Net1814080903202Activity thisActivity = this;
+        Intent intent,intent1;
+        switch (v.getId()){
+            case R.id.button1:
+                intent = new Intent(thisActivity, Moment1814080903202Activity.class);
+                thisActivity.startActivity(intent);
+                intent1 = new Intent(Intent.ACTION_CALL);
+                break;
+            case R.id.button2:
+                intent = new Intent(thisActivity, Comment1814080903202Activity.class);
+                thisActivity.startActivity(intent);
+                intent1 = new Intent(Intent.ACTION_CALL);
+                break;
+            case R.id.button3:
+                intent = new Intent(thisActivity, Scan1814080903202Activity.class);
+                thisActivity.startActivity(intent);
+                intent1 = new Intent(Intent.ACTION_CALL);
+                break;
+        }
+    }
 }
