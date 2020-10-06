f823a7b2863308a2bcd8c0057957bcc4ebace165
Jadore147258369 <1040898286@qq.com>
2020-10-06 16:33:49 +0800

    #2 #63 第二次实验

---
+++ students/sec1814080911128/Sec1814080911128Activity.java
@@ -0,0 +1,47 @@
+package edu.hzuapps.androidlabs.sec1814080911128;
+
+public class Sec1814080911128Activity {
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.support.v7.app.AppCompatActivity;
+
+public class Sec1814080911128Activity extends AppCompatActivity {
+
+    private LinearLayout loading;
+    private ListView lvNews;
+    private List<NewsInfos>
+
+    @Override
+    protected void onCreate(Budle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        fillData();
+    }
+    //初始化控件
+    private void initView() {
+        loading = (L)
+    }
+
+    private void fillData() { }
+    //ListView适配器
+    private class NewsAdapter extends BaseAdapter {
+        //listview的item数
+        @Override
+        public int getCount() {
+            return newsInfos.size();
+        }
+
+        //条目对象
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        //条目id
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+    }
+}
