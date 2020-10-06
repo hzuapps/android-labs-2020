d7c18b94826d7f99886cc463c8cd5c69edf053d5
alllovetaken <1395359854@qq.com>
2020-10-06 17:22:55 +0800

    第二次实验

--- students/net1814080903316/Net1814080903316Activity.java
+++ students/net1814080903316/Net1814080903316Activity.java
@@ -1,5 +1,26 @@
-package edu.hzuapps
+package edu.hzuapps.androidlabs;
 
-public class Net1814080903316Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903316Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.net_1814080903316_activity);
+        }
+        public void jump(View view){
+            Intent intent=new Intent(this,ToolActivity.class);
+            startActivity(intent);
+
+    }
+
+
 
 }
