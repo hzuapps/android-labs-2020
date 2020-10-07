d0ae4daa9856d7f4f1617e6eb2d81fba0bfdc726
@xu_kai_yue <2878786711@qq.com>
2020-10-07 10:02:38 +0800

    #2 #168

---
+++ students/net1814080903114/addActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class addActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
--- students/net1814080903325/Net1814080903325Activity.java
+++
@@ -1,6 +0,0 @@
-package edu.hzuapps.androidlabs.net1814080903325
-
-public class Net1814080903325Activity{
-
-}
-
