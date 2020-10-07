e7ef3d5e4a854df62ec2ad2c1502072ea0131722
Thisko <1095665963@qq.com>
2020-10-07 09:33:58 +0800

    #2 #23 第二次实验

--- students/net1814080903102/Net1814080903102Activity.java
+++ students/net1814080903102/Net1814080903102Activity.java
@@ -1,7 +1,47 @@
-package edu.hzuapps.androidlabs.net1814080903102;
+package edu.hzuapps.androidlabs;
 
-public class Net1814080903102Activity {
-	public static void main(String args[]){
-		System.out.println("Hello World!");
-	}
-	}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903102Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Net1814080903102Activity thisActivity = this;
+
+        Button addbutton = (Button) findViewById(R.id.button_add);
+        addbutton.setOnClickListener(new View.OnClickListener(){//匿名类
+            @Override
+            public void onClick(View v){//打开
+                Intent intent = new Intent(thisActivity,addclass.class);
+                thisActivity.startActivity(intent);
+                //thisActivity.startActivityForResult();
+            }
+        });
+
+        Button setbutton = (Button) findViewById(R.id.button_setting);
+        setbutton.setOnClickListener(new View.OnClickListener(){//匿名类
+            @Override
+            public void onClick(View v){//打开
+                Intent intent = new Intent(thisActivity,setclass.class);
+                thisActivity.startActivity(intent);
+                //thisActivity.startActivityForResult();
+            }
+        });
+    }
+}
+
+//    class MyClickListener implements View.OnClickListener{//新类
+//        @Override
+//        public void onClick(View v){
+//
+//        }
+//
+//    }
