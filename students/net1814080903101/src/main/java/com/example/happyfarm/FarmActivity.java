39a876e407e012144e73899dab0e3a6321462c11
KoKiChiChyan <1521430304@qq.com>
2020-10-07 09:35:46 +0800

    #2 #129 第二次实验

---
+++ students/net1814080903101/src/main/java/com/example/happyfarm/FarmActivity.java
@@ -0,0 +1,37 @@
+package com.example.happyfarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm);
+        Button button=(Button)findViewById(R.id.tostore);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(FarmActivity.this, StoreActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        Button button2=(Button)findViewById(R.id.tostoreware);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(FarmActivity.this, StorewareActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
