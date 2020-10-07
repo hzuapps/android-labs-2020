e7ca076e0c68ca6ed87e460befe8a76b8c428677
lyRomantic <2814757819@qq.com>
2020-10-07 18:20:48 +0800

    #2 #32

---
+++ students/net1814080903121/app/src/main/java/edu/hzuapps/androidlabs/net1814080903121/net1814080903121Activity.java
@@ -0,0 +1,27 @@
+package edu.hzuapps.androidlabs.net1814080903121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class net1814080903121Activity extends AppCompatActivity {
+
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1=findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(net1814080903121Activity.this,记录联系人Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
