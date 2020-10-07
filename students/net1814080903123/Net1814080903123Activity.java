088293d85309cac8d26d01fc19911fe6b39f93e4
zqy1158 <1228242146@qq.com>
2020-10-08 00:28:29 +0800

    #2 #445 第二次实验

--- students/net1814080903123/Net1814080903123Activity.java
+++ students/net1814080903123/Net1814080903123Activity.java
@@ -1,6 +1,37 @@
-package edu.hzuapps.androidlabs.net1814080903123
+package com.example.myapplication;
 
-public class Net1814080903123Activity{
-	public static void main(String[] args){}
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View v) {
+                                       Intent it = new Intent();
+                                       it.setClass(MainActivity.this, SkipActivity.class);
+                                       MainActivity.this.startActivity(it);
+                                   }    });
+
+            Button btn1= findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent it=new Intent();
+                    it.setClass( MainActivity.this, SkipActivity1.class);
+                    MainActivity.this.startActivity(it);
+                }
+
+        });
+    }
 }
---
+++ students/net1814080903123/SkipActivity1.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SkipActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skip_layout1);
+    }
+}
