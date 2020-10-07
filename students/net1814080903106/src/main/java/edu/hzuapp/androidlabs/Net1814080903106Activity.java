d24ae06c8019cf841797204f20abb726a3ea4d0c
ValiantRex <599097892@qq.com>
2020-10-07 12:19:21 +0800

    #2 #66 实验2

---
+++ students/net1814080903106/src/main/java/edu/hzuapp/androidlabs/Net1814080903106Activity.java
@@ -0,0 +1,40 @@
+package edu.hzuapp.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903106Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonToGame = (Button)findViewById(R.id.toGame);
+        buttonToGame.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                Intent intent = new Intent();
+                                                intent.setClass(Net1814080903106Activity.this, GameActivity.class);
+                                                startActivity(intent);
+                                            }
+                                        }
+        );
+
+
+        Button buttonToHistoryScore = (Button)findViewById(R.id.toHistoryScore);
+        buttonToHistoryScore.setOnClickListener(new View.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(View v) {
+                                                        Intent intent = new Intent();
+                                                        intent.setClass(Net1814080903106Activity.this, HistoryScoreActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                }
+        );
+    }
+}
