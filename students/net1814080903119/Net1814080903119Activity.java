efe0304aece7853ebc2e7e1bd0ef29a7dec05002
Lj-xinfei <1337275914@qq.com>
2020-10-07 22:54:54 +0800

    #2 #31 第2次实验

--- students/net1814080903119/Net1814080903119Activity.java
+++ students/net1814080903119/Net1814080903119Activity.java
@@ -1,5 +1,25 @@
-package edu.hzuapps.androidlabs.net1814080903119
+package edu.hzuapps.androidlabs.net1814080903119;
 
-public class Net1814080903119Activity{
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+public class Net1814080903119Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        final Net1814080903119Activity _this = this;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_this, FirstActivity.class);
+                _this.startActivity(intent);
+            }
+        });
+    }
 }
