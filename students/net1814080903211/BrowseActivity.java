2fcc7827634e881c6963af3674a8573177adb01a
Xterisk <liffey.tech@outlook.com>
2020-10-08 19:56:16 +0800

    #2 #11 第二次实验

---
+++ students/net1814080903211/BrowseActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903211;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrowseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse);
+    }
+}
--- students/net1814080903211/Net1814080903211Activity.java
+++ students/net1814080903211/Net1814080903211Activity.java
@@ -1,7 +1,72 @@
 package edu.hzuapps.androidlabs.net1814080903211;
 
-public class Net1814080903211Activity {
-    static public void main(String[] args) {
-        System.out.println("Confusing Instructions are error-prone, you know that.");
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.hzuapps.androidlabs.net1814080903211.databinding.ActivityMainBinding;
+
+public class Net1814080903211Activity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void jumpToProfile(View v)
+    {
+        Intent newActivity = new Intent(this, ProfileActivity.class);
+        startActivity(newActivity);
+    }
+
+    public void jumpToBrowse(View v)
+    {
+        Intent newActivity = new Intent(this, BrowseActivity.class);
+        startActivity(newActivity);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
