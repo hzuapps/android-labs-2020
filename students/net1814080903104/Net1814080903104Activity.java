f9ac5f7f0be1165e06d5b1941f8f4f93e4d14439
3ho-y <972607089@qq.com>
2020-10-08 12:31:50 +0800

    第二次实验

--- students/net1814080903104/Net1814080903104Activity.java
+++ students/net1814080903104/Net1814080903104Activity.java
@@ -1,5 +1,41 @@
-package edu.hzuapps
+package edu.hzuapps.myapplication;
 
-public class Net1814080903104Activity{
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903104Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Net1814080903104Activity thisActivity = this;
+
+        Button button = (Button) findViewById(R.id.button_open);
+        button.setX(200f);
+        button.setY(200f);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(thisActivity,GetReward.class);
+                thisActivity.startActivity(intent);
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.button_search);
+        button.setX(450f);
+        button.setY(1100f);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(thisActivity,SearchMess.class);
+                thisActivity.startActivity(intent);
+            }
+        });
+    }
 }
