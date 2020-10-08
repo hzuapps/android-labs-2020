ada0de147c485d7861d3f88fbd7ae5e3963459de
Miraiiiii <974773303@qq.com>
2020-10-08 21:11:04 +0900

    #2 #158 第2次实验

---
+++ net1814080903113/NetActivity3.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903113;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net3);
+    }
+}
