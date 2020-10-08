3254222392b5b64fe27d29d5c5669060fa78a737
Hacker-LAM <hacker_lam@163.com>
2020-10-08 17:38:56 +0800

    #2 #426 第二次实验

--- students/net1814080903304/Net1814080903304Activity.java
+++ students/net1814080903304/Net1814080903304Activity.java
@@ -1,5 +1,43 @@
-package edu.hzuapps;
+package hzu.android.labs;
 
-public class Net1814080903304Activity {
-  
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Net1814080903304Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final Net1814080903304Activity thisActivity = this;
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity,funtion_next.class);
+                startActivity(intent);
+            }
+        });
+
+
+        final Net1814080903304Activity funtion_settingActivity = this;
+
+        Button button12 = findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(funtion_settingActivity,funtion_setting.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
 }
