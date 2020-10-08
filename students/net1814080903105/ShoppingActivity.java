d1fc45f63a4625ac6b76f9b489a0d6fb5a25fbd6
czj-105 <1115049565@qq.com>
2020-10-08 20:00:07 +0800

    #2 #30 第二次实验

---
+++ students/net1814080903105/ShoppingActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping);
+    }
+}
