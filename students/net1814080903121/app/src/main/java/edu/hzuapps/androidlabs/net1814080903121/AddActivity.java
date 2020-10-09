a1f9b5da09a25dc77908e4ba9fd3954814583370
15218047860 <71614824+15218047860@users.noreply.github.com>
2020-10-09 18:28:49 +0800

    Update AddActivity.java

--- students/net1814080903121/app/src/main/java/edu/hzuapps/androidlabs/net1814080903121/AddActivity.java
+++ students/net1814080903121/app/src/main/java/edu/hzuapps/androidlabs/net1814080903121/AddActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class 记录联系人Activity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
