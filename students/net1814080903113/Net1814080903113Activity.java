e6c41042764f33b5466fca2aad4b29726848895f
Miraiiiii <974773303@qq.com>
2020-10-09 15:06:07 +0900

    删除不必要的文件

--- students/net1814080903113/Net1814080903113Activity.java
+++
@@ -1,6 +0,0 @@
-package edu.hzuapps.androidlabs.net1814080903113;
-public class Net1814080903113Activity{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
