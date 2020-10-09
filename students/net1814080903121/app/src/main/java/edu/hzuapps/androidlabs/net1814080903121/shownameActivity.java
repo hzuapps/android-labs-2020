768170b79e08d67f205b0ea5137b5178e36fe260
lyRomantic <2814757819@qq.com>
2020-10-09 14:50:25 +0800

    #2 #32 第二次实验

---
+++ students/net1814080903121/app/src/main/java/edu/hzuapps/androidlabs/net1814080903121/shownameActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class 显示联系人Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
