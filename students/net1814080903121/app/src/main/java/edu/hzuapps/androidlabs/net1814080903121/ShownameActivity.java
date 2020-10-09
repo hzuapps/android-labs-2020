eae42af5a9609482486e3a470329677cde56dae3
15218047860 <71614824+15218047860@users.noreply.github.com>
2020-10-09 18:30:12 +0800

    Update ShownameActivity.java

--- students/net1814080903121/app/src/main/java/edu/hzuapps/androidlabs/net1814080903121/ShownameActivity.java
+++ students/net1814080903121/app/src/main/java/edu/hzuapps/androidlabs/net1814080903121/ShownameActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class 显示联系人Activity extends AppCompatActivity {
+public class ShownameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
