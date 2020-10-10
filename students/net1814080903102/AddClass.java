204536529ed1e495f572ac3ce28723cfd77911da
Thisko <46182900+Thisko@users.noreply.github.com>
2020-10-10 22:42:12 +0800

    hzuapps#2 hzuapps#23 第二次实验

--- students/net1814080903102/AddClass.java
+++ students/net1814080903102/AddClass.java
@@ -10,5 +10,6 @@ public class AddClass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addclass);
+        
     }
 }
