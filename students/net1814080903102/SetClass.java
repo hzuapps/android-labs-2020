71813d4c01650ce86120c53e34b5643e32bdf3b1
Thisko <46182900+Thisko@users.noreply.github.com>
2020-10-10 22:43:01 +0800

    hzuapps#2 hzuapps#23 第二次实验

--- students/net1814080903102/SetClass.java
+++ students/net1814080903102/SetClass.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class setclass extends AppCompatActivity {
+public class SetClass extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
