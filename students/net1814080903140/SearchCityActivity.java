fc75e43512f3c11fe9229ad5ae025964d0c71c98
bowwran <779947085@qq.com>
2020-10-10 19:21:50 +0800

    #20 第二次实验

---
+++ students/net1814080903140/SearchCityActivity.java
@@ -0,0 +1,14 @@
+package com.example.boww;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_city);
+    }
+}
