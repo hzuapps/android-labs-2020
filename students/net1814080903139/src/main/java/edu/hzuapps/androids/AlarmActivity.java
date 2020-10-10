c4a7292d3d2e140c9bc7353ff70582b61e45b413
BBsan13 <1135963887@qq.com>
2020-10-10 18:24:53 +0800

    #1 #420 第二次实验

---
+++ students/net1814080903139/src/main/java/edu/hzuapps/androids/AlarmActivity.java
@@ -0,0 +1,12 @@
+package edu.hzuapps.androids;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
