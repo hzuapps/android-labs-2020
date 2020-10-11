7eca54231f3161c4d176b5b4dbb081bb057ee07a
xiaocheng123785 <2698217197@qq.com>
2020-10-11 21:37:36 +0800

    #2 #101 第2次实验

---
+++ students/net1814080903318/main/java/com/example/myapplication/MyVideoActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_video);
+    }
+}
