53c371d7a20f14fbe97c53d31132aee81833b829
Garfield-zheng <1974637297@qq.com>
2020-10-11 17:26:27 +0800

    #2 #263 第二次实验

---
+++ students/net1814080903328/Funtion_Next.java
@@ -0,0 +1,14 @@
+package huz.android.labs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Funtion_Next extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funtion__next);
+    }
+}
--- students/net1814080903328/Net1814080903328Activity.java
+++ students/net1814080903328/Net1814080903328Activity.java
@@ -1,5 +1,30 @@
-public class Net1814080903328Activity{
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-	}
+package huz.android.labs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903328Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final Net1814080903328Activity thisActivity = this;
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(thisActivity, Funtion_Next.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
