20c733003eeb0b6c5b26eec60937844d2cee3b15
Classic-Z <951473204@qq.com>
2020-10-11 23:14:45 +0800

    #2 #281 第二次实验

---
+++ students/net1814080903321/StoreActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StoreActivity.this, ShowActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
