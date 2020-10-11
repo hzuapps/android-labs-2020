d4500215822f8057f47368ddcc726430ec506115
Classic-Z <951473204@qq.com>
2020-10-11 23:22:16 +0800

    #2 #281 第二次实验

--- students/net1814080903321/Net1814080903321Activity.java
+++ students/net1814080903321/Net1814080903321Activity.java
@@ -1,5 +1,28 @@
-public class Net1814080903321Activity { 
- static public void main(String[] args) { 
- System.out.println("hello world."); 
- } 
- } 
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903321Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903321);
+
+
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Net1814080903321Activity.this, StoreActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
