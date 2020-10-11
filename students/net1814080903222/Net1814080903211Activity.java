4e3b8f240304b991fc942dcd7ec47a6fdfce7ffa
zhongzhitao <liuhaihuachiji@163.com>
2020-10-11 22:40:45 +0800

    #2 #312 第二次实验

--- students/net1814080903222/Net1814080903211Activity.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.net1814080903222;
-
-public class Net1814080903222Activity.java {
-	static public void Main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
---
+++ students/net1814080903222/Net1814080903222Activity.java
@@ -0,0 +1,51 @@
+package edu.hzuapps.androidlabs.net1814080903222;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import edu.hzuapps.androidlabs.net1814080903222.databinding.ActivityMainBinding;
+
+public class Net1814080903222Activity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void jumpToFolder(View v)
+    {
+        Intent intent = new Intent(this, FolderActivity.class);
+        startActivity(intent);
+    }
+
+    public void jumpToSettings(View v)
+    {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+}
