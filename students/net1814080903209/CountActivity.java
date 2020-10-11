c6dcbcc003523d8d21a147681a463d0829fe9d76
itgopan <itgopan@foxmail.com>
2020-10-11 23:53:57 +0800

    #2 #21 第2次实验

---
+++ students/net1814080903209/CountActivity.java
@@ -0,0 +1,16 @@
+package com.gopan.bills.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gopan.bills.R;
+
+public class CountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count);
+    }
+}
--- students/net1814080903209/Net1814080903209Activity.java
+++ students/net1814080903209/Net1814080903209Activity.java
@@ -1,7 +1,41 @@
-public Net1814080903209Activity {
-	public static void main(String[] args) {
-        // $ git add students/net1814080903209/Net1814080903209Activity.java
-        // $ git commit -m "#1 #21 第1次实验" 
-        // $ git push
+package com.gopan.bills.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.gopan.bills.R;
+
+public class Net1814080903209Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Net1814080903209Activity thisActivity = this;
+
+        Button btn_count = findViewById(R.id.btn_count);
+        btn_count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity, CountActivity.class);
+                thisActivity.startActivity(intent);
+                Intent intent1 = new Intent(Intent.ACTION_VIEW);
+            }
+        });
+
+        Button btn_show = findViewById(R.id.btn_show);
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity, ShowActivity.class);
+                thisActivity.startActivity(intent);
+                Intent intent2 = new Intent(Intent.ACTION_VIEW);
+            }
+        });
     }
 }
