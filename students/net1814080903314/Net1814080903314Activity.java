4bef3ffde9f57d96077c9fe5ab2164acc616b7b3
Aya <AzStartie@qq.com>
2020-10-12 00:45:09 +0800

    #2 #266 第二次实验

--- students/net1814080903314/Net1814080903314Activity.java
+++ students/net1814080903314/Net1814080903314Activity.java
@@ -1,9 +1,24 @@
-﻿public class Net1814080903314Activity{
+package edu.hzuapps.androidlabs.net1814080903314;
 
- public static void main(String[] args){
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-  System.out.println("Haro warudo!");
-
- }
-
+public class Net1814080903314Activity extends AppCompatActivity {
+private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btn_button);
+        button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Net1814080903314Activity.this , ButtonActivity.class);
+            startActivity(intent);
+    }
+});
+    }
 }
