91a5bb91b5d461658e950d37d2e20539c3be8f77
unknown <2258559889@qq.com>
2020-10-12 01:15:15 +0800

    #2 #309 第2次实验

---
+++ students/net1814080903323/Net1814080903323Activity.java
@@ -0,0 +1,39 @@
+package com.example.net1814080903323;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903323Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903323);
+
+
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Net1814080903323Activity.this, InvitationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(Net1814080903323Activity.this, TipsActivity.class);
+                startActivity(j);
+            }
+        });
+
+
+    }
+}
