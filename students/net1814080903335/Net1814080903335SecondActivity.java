abba27db1299f06e95a11195871f5a9fdecb2133
forever-young-hh <1198691257@qq.com>
2020-10-11 18:22:29 +0800

    #2 #419 第二次实验

---
+++ students/net1814080903335/Net1814080903335SecondActivity.java
@@ -0,0 +1,14 @@
+package huz.android.net1814080903335;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Net1814080903335SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903335_second);
+    }
+}
