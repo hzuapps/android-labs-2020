4b36fc07c90f6eab82e63c6b16150609c67d0390
KoKiChiChyan <1521430304@qq.com>
2020-10-11 20:38:41 +0800

    #2 #129 第二次实验

--- students/net1814080903101/src/main/java/edu/hzuapps/androidlabs/Soft1814080903101/Soft1814080903101Activity.java
+++ students/net1814080903101/src/main/java/edu/hzuapps/androidlabs/Soft1814080903101/Soft1814080903101Activity.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Net1814080903101Activity extends AppCompatActivity {
+public class Soft1814080903101Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Net1814080903101Activity thisActivity=this;
+        final Soft1814080903101Activity thisActivity=this;
         Button button=(Button)findViewById(R.id.button_add);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(Net1814080903101Activity.this, FarmActivity.class);
+                intent.setClass(Soft1814080903101Activity.this, FarmActivity.class);
                 startActivity(intent);
 
             }
