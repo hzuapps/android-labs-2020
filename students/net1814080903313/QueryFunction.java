231942a181010249cc4a071e0dab1eed1975e0b3
rsdczhs <916811372@qq.com>
2020-10-12 12:38:46 +0800

    #2 #499 第二次实验

--- students/net1814080903313/QueryFunction.java
+++ students/net1814080903313/QueryFunction.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity2 extends AppCompatActivity {
+public class QueryFunction extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
