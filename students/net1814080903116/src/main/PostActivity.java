8c828c3e9b1b3bde5b94e896516184a702af7bb3
Dlmdp <770452043@qq.com>
2020-10-12 23:31:53 +0800

    #2 #122 第二次实验

---
+++ students/net1814080903116/src/main/PostActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class postActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
