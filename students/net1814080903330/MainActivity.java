b610d50d0a7a5bbe72d57c7a89163a1b56b04729
unknown <925012673@qq.com>
2020-10-12 15:52:00 +0800

    #2 #73 第二次实验

---
+++ students/net1814080903330/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.net1814080903330;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Button button2;
+    private Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, playActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, collectionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, searchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ students/net1814080903330/Net1814080903330Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps;
+
+public class Net1814080903330Activity{
+	public static void main(String[] args) {
+		
+	}
+}
