939597252a227afbc5de056e24c0a7e59ce9657e
Your Name <2627044316@qq.com>
2020-10-12 11:23:03 +0800

    #2 #80 第二次实验

---
+++ students/net1814080903234/ContactActivity.java
@@ -0,0 +1,28 @@
+package edu.hzuapp.androidlabs.net1814080903234;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        final Activity thisActivity = this;
+
+        Button btnMian = (Button) findViewById(R.id.net);
+        btnMian.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity,Net1814080903234Activity.class);
+                thisActivity.startActivity(intent);
+            }
+        });
+    }
+}
--- students/net1814080903234/Net1814080903234Activity.java
+++ students/net1814080903234/Net1814080903234Activity.java
@@ -1,5 +1,44 @@
-package edu.hzuapps.androidlabs.net1814080903234
+package edu.hzuapp.androidlabs.net1814080903234;
 
-public class Net1814080903234Activity {
-	
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903234Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903234);
+
+        final Activity thisActivity = this;
+
+        Button btnCon = (Button) findViewById(R.id.contact);
+
+        btnCon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity,ContactActivity.class);
+                thisActivity.startActivity(intent);
+            }
+        });
+
+        Button btnDyn = (Button) findViewById(R.id.dynamic);
+
+        btnDyn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(thisActivity,DynamicActivity.class);
+                thisActivity.startActivity(intent1);
+            }
+        });
+
+    }
+
+
+
+
 }
