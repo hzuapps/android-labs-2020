4e857016772036b229d245ef2bc8b6be42a5e10c
tiamo669 <crd19806630636>
2020-10-12 13:54:14 +0800

    #2 #59 第二次实验

---
+++ students/net1814080903303/StudentActivity.java
@@ -0,0 +1,16 @@
+package com.example.edu.hzuapps.androidlabs.net1814080903303;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infomation);
+    }
+}
