3aac421db7d0703563f9539190fc3b4520eaca35
JeesionChone <1103882316@qq.com>
2020-10-12 14:16:21 +0800

    #2 #53 第二次实验

---
+++ students/net1814080903310/First18148080903310Activity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.students.net1814080903310;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class First18148080903310Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first18148080903310);
+    }
+}
