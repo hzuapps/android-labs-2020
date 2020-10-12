2adf26542942aa4c11e3cc0db72e8eb217ae4726
Classic-Z <951473204@qq.com>
2020-10-12 14:30:49 +0800

    #2 #281 第二次实验

--- students/net1814080903321/Net1814080903321Activity.java
+++ students/net1814080903321/Net1814080903321Activity.java
@@ -1,11 +1,11 @@
-package com.example.myapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
+package edu.hzuapp.androidlabs.Net1814080903321;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Net1814080903321Activity extends AppCompatActivity {
 
--- students/net1814080903321/ShowActivity.java
+++ students/net1814080903321/ShowActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package edu.hzuapp.androidlabs.Net1814080903321;
 
 import androidx.appcompat.app.AppCompatActivity;
 
