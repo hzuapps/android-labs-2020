8a28740de522f50f632469e1ffcb4f86900dff2c
L1nzSec <1227812331@qq.com>
2020-10-12 16:49:15 +0800

    #2 #34 第2次实验

---
+++ students/sec1814080911202/BorrowActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.sec1814080911202;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BorrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_borrow);
+    }
+}
