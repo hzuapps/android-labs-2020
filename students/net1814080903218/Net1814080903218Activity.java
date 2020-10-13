344b9de12c995c58c2fef882ddd4d185abaec2b2
Wumuuu <995150275@qq.com>
2020-10-13 17:02:53 +0800

    第二次实验

--- students/net1814080903218/Net1814080903218Activity.java
+++ students/net1814080903218/Net1814080903218Activity.java
@@ -1,7 +1,26 @@
 package edu.hzuapps.androidlabs.net1814080903218;
 
-public class Net1814080903218Activity {
-    static public void main(String[] args) {
-        System.out.println("holle world.");
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Net1814080903218Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903218);
+        Button button = findViewById(R.id.button);
+        final Net1814080903218Activity _this = this;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_this, AskForLeave1814080903218Activity.class);
+                _this.startActivity(intent);
+            }
+        });
     }
 }
