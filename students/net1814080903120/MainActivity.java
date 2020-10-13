a5075000c3871c5302360d419b04aa117b7b98cb
lyRomantic <1215884207@qq.com>
2020-10-13 22:52:42 +0800

    #2 #第二次实验

---
+++ students/net1814080903120/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void skip(View view){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this,OtherActivity.class);
+        startActivity(intent);
+    }
+}
--- students/net1814080903120/Net1814080903120Activity.java
+++ students/net1814080903120/Net1814080903120Activity.java
@@ -1,6 +1,16 @@
-public class Net1814080903120Activity{
+package com.example.myapplication;
 
-		public static void main(String[] args){
-		System.out.println("Hi");
-	}
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Net1814080903120Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.other);
+    }
 }
