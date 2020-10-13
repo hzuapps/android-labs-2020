5ab642ab218a78160a43d056853dafbbce598672
ccc2020916 <1713624582@qq.com>
2020-10-13 23:41:29 +0800

    #2 #131 第二次实验

---
+++ students/net1814080903132/AskActivity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidlabs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask);
+    }
+}
