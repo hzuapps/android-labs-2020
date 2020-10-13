276cba0dc4be7756a35d12a2db82cc168e9c4a21
Clever2app <879907167@qq.com>
2020-10-13 11:28:48 +0800

    #2 #107 第2次实验

--- students/net1814080903240/bindingActivity.java
+++ students/net1814080903240/bindingActivity.java
@@ -1,30 +1,27 @@
 package edu.hzuapps.androidlabs.net1814080903240;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class bindingActivity extends AppCompatActivity {
 
-public class Net1814080903240Activity extends AppCompatActivity {
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_binding);
+        Button button = findViewById(R.id.button2);
 
-        final Net1814080903240Activity thisActivity = this;
-
-        Button button = (Button) findViewById(R.id.button1);
+        final bindingActivity this1 = this;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(thisActivity,bindingActivity.class);
+                Intent intent = new Intent(this1,installActivity.class);
                 startActivity(intent);
             }
         });
-
     }
-
 }
