74e8e477a5273dce5c8ec77fab571f9d92b52465
HamburgerBB <917608458@qq.com>
2020-10-13 23:20:30 +0800

    #2 #511 第二次实验

--- students/net1814080903128/net1814080903128Activity.java
+++ students/net1814080903128/net1814080903128Activity.java
@@ -1,5 +1,33 @@
-package edu.hzuapps.androidlabs.net1814080903220
+package edu.hzuapps.androidlabs.Net1814080903128.cookcake;
 
-public class Net1814080903128activity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+
+public class Net1814080903128Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    final Activity thisActivity = this;
+
+    Button button = (Button) findViewById(R.id.cake);
+
+    View view = findViewById(R.id.cake);
+
+        button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(thisActivity, CakeActivity.class);
+            thisActivity.startActivity(intent);
+        }
+    });
+
+
+}
 }
