eb004a9c821aceafcd0b25ceb67a3a57b63cfacb
Howard-SSS <60313659+Howard-SSS@users.noreply.github.com>
2020-10-13 16:28:33 +0800

    #2 #27 第二次实验

--- students/net1814080903107/Command.java
+++ students/net1814080903107/Command.java
@@ -3,4 +3,9 @@ package com.example.thefirst;
 import android.app.Activity;
 
 public class Command extends Activity {
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_command);
+    }
 }
