b00dc5061a18d627fb231c010799f75e4f39c613
unknown <1481923078@qq.com>
2020-10-13 22:08:33 +0800

    #1 #421 第2次实验

--- students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/Net1814080903141Activity.java
+++ students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/Net1814080903141Activity.java
@@ -1,13 +1,13 @@
-package edu.hzuapps.androidlabs.net1814080903141;
+package edu.hzuapp.androidlabs.net1814080903141;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Net1814080903141Activity extends AppCompatActivity{
+public class Net1814080903141Activity extends AppCompatActivity {
     private Net1814080903141Activity myThis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +24,8 @@ public class Net1814080903141Activity extends AppCompatActivity{
         });
         recordButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent recordIntent=new Intent(myThis,RecordActivity.class);
-                startActivity(recordIntent);
+                Intent newIntent=new Intent(myThis,RecordActivity.class);
+                startActivity(newIntent);
             }
         });
     }
--- students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/RecordActivity.java
+++ students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/RecordActivity.java
@@ -1,14 +1,13 @@
-package edu.hzuapps.androidlabs.net1814080903141;
+package edu.hzuapp.androidlabs.net1814080903141;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecordActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
     }
 }
