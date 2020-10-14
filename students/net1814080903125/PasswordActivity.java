48c2d4fb6d4594c100c9fb8d05175c43ba8c3601
zkw55 <1352617067@qq.com>
2020-10-14 21:03:06 +0800

    第三次实验

--- students/net1814080903125/PasswordActivity.java
+++ students/net1814080903125/PasswordActivity.java
@@ -3,9 +3,11 @@ package edu.hzuapps.androidlabs.net1814080903125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PasswordActivity extends AppCompatActivity {
 
@@ -14,9 +16,8 @@ public class PasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
 
-        Button back_btn = (Button)findViewById(R.id.delay_btn);
-        back_btn.setX(600f);
-        back_btn.setY(200f);
+        //设置返回按钮的跳转
+        Button back_btn = (Button)findViewById(R.id.back_btn2);
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +25,11 @@ public class PasswordActivity extends AppCompatActivity {
                 PasswordActivity.this.startActivity(intent);
             }
         });
+
+        //设置TextView的大小及位置
+        Drawable drawable = getResources().getDrawable(R.drawable.lock);
+        drawable.setBounds(0,0,1100,700);
+        TextView view = findViewById(R.id.lock);
+        view.setCompoundDrawables(drawable,null,null,null);
     }
 }
