be4ff1e5c08edbc57bf5725aa0cac5552666616d
Howard-SSS <60313659+Howard-SSS@users.noreply.github.com>
2020-10-14 20:27:14 +0800

    Update and rename Search.java to SearchActivity.java

--- students/net1814080903107/SearchActivity.java
+++ students/net1814080903107/SearchActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Search extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
