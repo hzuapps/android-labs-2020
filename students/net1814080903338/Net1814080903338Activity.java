2ef559b3364d67c898ba2fceb6f08510d0d51df4
unknown <2360680613@qq.com>
2020-10-14 22:19:36 +0800

    #2 #251 第二次实验

--- students/net1814080903338/Net1814080903338Activity.java
+++ students/net1814080903338/Net1814080903338Activity.java
@@ -1,6 +1,23 @@
-package edu.hzuapps.androidlabs
+package edu.hzuapps;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Net1814080903338Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
+public class Net1814080903338Activity extends AppCompatActivity {
+    public void onClickRead(View view) {
+        Intent intent = new Intent(this, ReadActivity.class);
+        startActivity(intent);
+    }
+    public void onClickWrite(View view) {
+        Intent intent = new Intent(this,WriteActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903338);
+    }
 }
