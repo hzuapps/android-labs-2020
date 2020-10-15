49b32c2b84f03cb24bd8cb5ade9b4ff3e938dc2e
LAPTOP-HPL6MM6C\czd <1148026508@qq.com>
2020-10-15 22:18:57 +0800

    #3 #142 第三次实验

--- students/net1814080903117/app/src/main/java/edu/hzuapps/androidlabs/net1814080903117/DownloadActivity.java
+++ students/net1814080903117/app/src/main/java/edu/hzuapps/androidlabs/net1814080903117/DownloadActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class DownloadActivity extends AppCompatActivity {
 
@@ -24,5 +26,10 @@ public class DownloadActivity extends AppCompatActivity {
                 thisActivity.finish();
             }
         });
+
+        Drawable drawable = getResources().getDrawable(R.drawable.download);
+        drawable.setBounds(0,0,200,200);
+        TextView textview = findViewById(R.id.textview_01);
+        textview.setCompoundDrawables(null,drawable,null,null);
     }
 }
--- students/net1814080903117/app/src/main/java/edu/hzuapps/androidlabs/net1814080903117/UploadActivity.java
+++ students/net1814080903117/app/src/main/java/edu/hzuapps/androidlabs/net1814080903117/UploadActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -24,5 +26,9 @@ public class UploadActivity extends AppCompatActivity {
                 thisActivity.finish();
             }
         });
+        Drawable drawable = getResources().getDrawable(R.drawable.upload);
+        drawable.setBounds(0,0,200,200);
+        TextView textview = findViewById(R.id.textview_01);
+        textview.setCompoundDrawables(null,drawable,null,null);
     }
 }
