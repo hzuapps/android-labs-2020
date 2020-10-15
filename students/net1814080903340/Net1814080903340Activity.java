0275e19c26699379b3298639893219a520ba69f1
unknown <1142455296@qq.com>
2020-10-15 19:46:59 +0800

    #2 #272 第2次实验

--- students/net1814080903340/Net1814080903340Activity.java
+++ students/net1814080903340/Net1814080903340Activity.java
@@ -1,6 +1,31 @@
-package edu.hzuapps.androidlabs.net1814080903340;
-public class Net1814080903340Activity {
-    public static void main(String args[]){
-        System.out.println("Hello World!");
+package edu.hzuapps.net1814080903340activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Net1814080903340Activity extends AppCompatActivity {
+
+        public void onClickEasy(View view) {
+            Intent intent = new Intent(this, Easy.class);
+            startActivity(intent);
+        }
+        public void onClickMiddle(View view) {
+            Intent intent = new Intent(this,Middle.class);
+            startActivity(intent);
+        }
+
+        public void onClickHigh(View view) {
+            Intent intent = new Intent(this,High.class);
+            startActivity(intent);
+        }
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_net1814080903340);
+        }
     }
-}
