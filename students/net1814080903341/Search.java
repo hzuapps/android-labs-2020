69a48fb6f98b8f7c827dd37a283b0e02ccdde3e2
ting-man <1571884920@qq.com>
2020-10-16 21:20:02 +0800

    #2 #262 第2次实验

---
+++ students/net1814080903341/Search.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.Net1814080903341Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
