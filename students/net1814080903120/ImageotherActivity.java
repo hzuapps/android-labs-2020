53e0aac8ffd558bfd4110e0145effe033b8c290f
lyRomantic <1215884207@qq.com>
2020-10-17 12:29:14 +0800

    #3 #595 第三次实验

---
+++ students/net1814080903120/ImageotherActivity.java
@@ -0,0 +1,20 @@
+package edu.hzuapps.androidlabs.net1814080903120;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+public class ImageotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_actother);
+
+    }
+
+}
