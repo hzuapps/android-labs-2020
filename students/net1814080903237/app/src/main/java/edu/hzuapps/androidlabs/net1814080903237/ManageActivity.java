77d19d5248df3ebbd8e0106c7260f6ae8459fd97
liangyunrong <2469221265@qq.com>
2020-10-18 21:44:29 +0800

    #2 #363  第二次实验

---
+++ students/net1814080903237/app/src/main/java/edu/hzuapps/androidlabs/net1814080903237/ManageActivity.java
@@ -0,0 +1,11 @@
+package edu.hzuapps.androidlabs.net1814080903237;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class ManageActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage);
+    }
+}
+
