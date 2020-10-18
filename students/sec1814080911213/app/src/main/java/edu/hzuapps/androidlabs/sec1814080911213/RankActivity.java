6ae7a66c29a9fba5dbf02d15c87c66077ed4f403
uzi0000 <907955651@qq.com>
2020-10-19 01:49:43 +0800

    #2 #283 第二次实验

---
+++ students/sec1814080911213/app/src/main/java/edu/hzuapps/androidlabs/sec1814080911213/RankActivity.java
@@ -0,0 +1,30 @@
+package edu.hzuapps.androidlabs.sec1814080911213;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class RankActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final Activity thisActivity = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank);
+        Button btnOpen1 = (Button) findViewById(R.id.main_btn);
+        btnOpen1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity, sec1814080911213Activity.class);
+                thisActivity.startActivity(intent);
+                thisActivity.finish();
+            }
+        });
+    }
+}
