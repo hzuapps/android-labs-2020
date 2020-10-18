ead370d892562ec4e2e435b1058510b67bb6d2ac
holenchan <1370227996@qq.com>
2020-10-18 23:13:57 +0800

    #2 #277 第2次实验

---
+++ students/sec1814080911111/app/src/main/java/sec1814080911111/CountdownTimerActivity.java
@@ -0,0 +1,13 @@
+package com.example.sec1814080911111;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountdownTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown_timer);
+    }
+}
---
+++ students/sec1814080911111/app/src/main/java/sec1814080911111/Sec1814080911111Activity.java
@@ -0,0 +1,13 @@
+package com.example.sec1814080911111;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sec1814080911111Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
