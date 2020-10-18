fb3b23a6c78d17024cb1a6c7fa0b301c67720e2f
nan-del <1623605987@qq.com>
2020-10-18 17:45:32 +0800

    #2 #319 第2次实验

---
+++ students/sec1814080911131/Third_Activity.java
@@ -0,0 +1,14 @@
+package com.example.sec1814080911131activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Third_Activity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout_activity);
+    }
+}
