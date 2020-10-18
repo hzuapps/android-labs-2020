3b15d8c65c097fb6ffb83d41cbfb38ed9f5afcb0
1nnocent1 <980044012@qq.com>
2020-10-18 23:54:26 +0800

    #2 #323 #第二次实验

---
+++ students/sec1814080911125/FistActvity.java
@@ -0,0 +1,18 @@
+package com.example.sec1814080911125;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sec1814080911125.R;
+
+public class FistActvity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fist_actvity);
+    }
+}
---
+++ students/sec1814080911125/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.sec1814080911125;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
