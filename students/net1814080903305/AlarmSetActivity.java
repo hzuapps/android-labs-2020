eb0673d8a03e67579571f60512f1ddec4d7102c6
bakamaid777 <ero_shou_meido@qq.com>
2020-10-18 21:36:49 +0800

    #2 #57 第2次实验

---
+++ students/net1814080903305/AlarmSetActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_set);
+    }
+}
---
+++ students/net1814080903305/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.android_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final MainActivity thisActivity=this;
+        Button btnitem = (Button) findViewById(R.id.button);
+        btnitem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity,AlarmSetActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btnread = (Button) findViewById(R.id.button1);
+        btnread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity,TimeDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
