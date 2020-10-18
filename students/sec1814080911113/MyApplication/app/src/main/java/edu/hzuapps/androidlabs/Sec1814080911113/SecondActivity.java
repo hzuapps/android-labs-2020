bf42a520bd3fa523a9432b8c28e8377dd6991e80
BlaiseChin <13059365076@163.com>
2020-10-18 23:00:49 +0800

    #2 #37 第二次实验

---
+++ students/sec1814080911113/MyApplication/app/src/main/java/edu/hzuapps/androidlabs/Sec1814080911113/SecondActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.Sec1814080911113;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
