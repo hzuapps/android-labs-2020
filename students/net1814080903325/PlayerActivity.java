f0a80de71edf1e32aaccd499babc7304a24a67e3
HuaStarOS <2532933005@qq.com>
2020-10-19 15:46:06 +0800

    #3 #56 第三次实验

---
+++ students/net1814080903325/PlayerActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903325;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903325);
+    }
+}
