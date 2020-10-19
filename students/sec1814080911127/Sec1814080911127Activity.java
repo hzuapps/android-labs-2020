30df96cfebcd1d94db346d021e1c7a05c7018478
Dokemg <1921648231@qq.com>
2020-10-19 11:40:21 +0800

    #2 #44 提交实验2

--- students/sec1814080911127/Sec1814080911127Activity.java
+++ students/sec1814080911127/Sec1814080911127Activity.java
@@ -1,11 +1,45 @@
-package edu.hzuapps.androidlabs.sec1814080911127;
+package androidlabs.hzuapps.edu.sec1814080911127;
 
-/**
- * Created by k00 on 2020/9/28.
- */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Sec1814080911127Activity {
-    public static void main(String[] args) {
+import androidx.annotation.Nullable;
+
+public class Sec1814080911127Activity extends Activity {
+
+    private Button button;
+    private View.OnClickListener onClickListener;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.button:
+                        Intent intent = new Intent(Sec1814080911127Activity.this,AddActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.button2:
+                        Intent intent1 = new Intent(Sec1814080911127Activity.this,AddActivity.class);
+                        startActivity(intent1);
+                        break;
+                }
+
+            }
+        };
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(onClickListener);
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(onClickListener);
 
     }
 }
