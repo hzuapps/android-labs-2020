ba4cfe8e6d2cc09bca506104eb5042fd482167e0
momingyu1 <429477910@qq.com>
2020-10-19 15:18:08 +0800

    #2 #259 第2次实验

--- students/net1814080903342/Net1814080903342Activity.java
+++ students/net1814080903342/Net1814080903342Activity.java
@@ -1,3 +1,26 @@
-package edu.hzuapps.androidlabs.net1814080903342;
-public class Net1814080903342Activity{
+package edu.hzuapps.androidlabs.Net1814080903342Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Net1814080903342Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1814080903342);
+    }
+
+    public void OnClickSearchId(View view) {
+        Intent intent = new Intent(this,SearchIdActivity.class);
+        startActivity(intent);
+    }
+
+    public void OnClickSearchTrading(View view) {
+        Intent intent = new Intent(this,SearchTradingActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ students/net1814080903342/SearchTradingActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.Net1814080903342Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchTradingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_trading);
+    }
+}
