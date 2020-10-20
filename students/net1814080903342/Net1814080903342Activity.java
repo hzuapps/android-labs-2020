1fdd47279a2c058e304e4bb1cca15a25cc971d64
momingyu1 <429477910@qq.com>
2020-10-20 09:02:41 +0800

    3

--- students/net1814080903342/Net1814080903342Activity.java
+++ students/net1814080903342/Net1814080903342Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Net1814080903342Activity extends AppCompatActivity {
 
@@ -12,15 +13,25 @@ public class Net1814080903342Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1814080903342);
-    }
+        final Net1814080903342Activity thisActicity = this;
 
-    public void OnClickSearchId(View view) {
-        Intent intent = new Intent(this,SearchIdActivity.class);
-        startActivity(intent);
-    }
+    Button btnOpen = (Button) findViewById(R.id.button);
+        btnOpen.setOnClickListener(new View.OnClickListener(){
 
-    public void OnClickSearchTrading(View view) {
-        Intent intent = new Intent(this,SearchTradingActivity.class);
-        startActivity(intent);
-    }
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActicity,SearchIdActivity.class);
+                thisActicity.startActivity(intent);
+            }
+    });
+
+        Button btnStart = (Button) findViewById(R.id.button2);
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActicity,SearchTradingActivity.class);
+                thisActicity.startActivity(intent);
+            }
+        });
+}
 }
