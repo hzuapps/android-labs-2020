4568d03a3259972fc012d11b328c542c96740e24
linzisong <744740666@qq.ocm>
2020-10-21 21:59:04 +0800

    #1 #2 #3 #613 第1、2、3次实验

--- students/sec1814080911238/Sec1814080911238Activity.java
+++ students/sec1814080911238/Sec1814080911238Activity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Sec1814080911238Activity extends AppCompatActivity {
     @Override
@@ -22,6 +23,7 @@ public class Sec1814080911238Activity extends AppCompatActivity {
                 thisActivity.startActivity(intent);
             }
         });
+
 
         Button btnOpen2 = (Button) findViewById(R.id.button2);
         btnOpen2.setOnClickListener(new View.OnClickListener() {
