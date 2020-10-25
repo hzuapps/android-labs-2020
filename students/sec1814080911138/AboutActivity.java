a76b5f8f42b33bc3fdad48cec88104b6e0ab6f89
1814080911138 <1244908079@qq.com>
2020-10-26 01:53:38 +0800

    #2 #405 第2次实验

---
+++ students/sec1814080911138/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.example.pomelo_note;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_view);
+    }
+
+
--- students/sec1814080911138/Sec1814080911138Activity.java
+++ students/sec1814080911138/Sec1814080911138Activity.java
@@ -1,7 +1,19 @@
-edu.hzuapps.androidlabs.sec1814080911138.Sec1814080911138Activity.java
+package com.example.pomelonote
 
-public class Sec1814080911138Activity{
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.List;
+import android.widget.Toast;
+import android.widget.DatePicker;
+importimport android.view.Menu; android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public static void main(String[ ] args){
-System.out.println("Hello World")
+public class MainActivity{
+
+    private List < CostBean > mCostBeanList;
+    mCostBeanList = new ArrayList<>();
+    ListView costList = (ListView) findViewById(R.id.lv_main);
+
+
 }
