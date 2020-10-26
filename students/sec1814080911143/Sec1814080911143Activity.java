02e7b07dd22efd647583028a32e23cc0bfc7cf54
1814080911143 <2317796431@qq.com>
2020-10-26 11:01:15 +0800

    #1 #734 第一次实验

---
+++ students/sec1814080911143/Sec1814080911143Activity.java
@@ -0,0 +1,18 @@
+edu.hzuapps.androidlabs.sec1814080911143.Sec1814080911143Activity.java  
+              
+  
+            
+  
+public class Sec1814080911143Activity{  
+              
+  
+              
+
+public static void main(String[ ] args){  
+              
+
+System.out.println("hello world")  
+              
+
+}  
+}
