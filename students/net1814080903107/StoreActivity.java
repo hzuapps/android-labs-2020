1c0717824dc914e205d08feb16090117f31b5474
Howard-SSS <2548795196@qq.com>
2020-10-27 20:33:31 +0800

    #4 #27 第四次实验

--- students/net1814080903107/StoreActivity.java
+++ students/net1814080903107/StoreActivity.java
@@ -2,16 +2,29 @@ package com.example.thefirst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.net.Inet4Address;
+
 public class StoreActivity extends AppCompatActivity {
+    boolean flag=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
-
+    }
+    public void click(View v){
+        Intent it=new Intent();
+        it.setType("video/*");
+        startActivityForResult(it,101);
+    }
+    protected void onActivityResult(int reqCode,int resCode,Intent data) {
+        super.onActivityResult(reqCode, resCode, data);
+        if (resCode == Activity.RESULT_OK)
+            flag=true;
     }
 }
