fef692c2af7ed29c13844b8982d564db32b516d8
huanxuaner <892937670@qq.com>
2020-10-28 10:00:39 +0800

    #4 #25 第4次实验

---
+++ students/net1814080903109/TurnoffScreenActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903109;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TurnoffScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.turnoffscreen);
+    }
+}
