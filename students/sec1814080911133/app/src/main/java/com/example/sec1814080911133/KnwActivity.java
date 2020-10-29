22cd73c9d333153a8cf50bc8951dbb88e327d6a8
张旻书 <475054603@qq.com>
2020-10-30 00:02:48 +0800

    #2 #197 第2次实验

---
+++ students/sec1814080911133/app/src/main/java/com/example/sec1814080911133/KnwActivity.java
@@ -0,0 +1,14 @@
+package com.example.sec1814080911133;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KnwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knw);
+    }
+}
