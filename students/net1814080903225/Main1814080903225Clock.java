85d32936472913756597d438ec69bb5d0f5f78df
ZYL618 <68884454+ZYL618@users.noreply.github.com>
2020-10-29 10:39:56 +0800

    Create Main1814080903225Clock.java

---
+++ students/net1814080903225/Main1814080903225Clock.java
@@ -0,0 +1,18 @@
+package edu.hzuapp.androidlabs.net1814080903225;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+
+public class Main1814080903225Clock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main1814080903225_clock);
+    }
+
+
+}
