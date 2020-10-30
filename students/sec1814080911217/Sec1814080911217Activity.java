94f6c66c450afa6cfe16e5bb513dcfe1dd0346e8
1364480222 <1364480222@qq.com>
2020-10-30 20:05:57 +0800

    #2 #38 第二次实验

--- students/sec1814080911217/Sec1814080911217Activity.java
+++ students/sec1814080911217/Sec1814080911217Activity.java
@@ -1,10 +1,10 @@
-package com.example.sec1814080911217;
+package edu.hzuapps.androidlabs.sec1814080911217;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,33 +15,34 @@ public class Sec1814080911217Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         final Activity thisActivity = this;
 
-        Button earn = (Button) findViewById(R.id.button_earnintegralactivity);
+        Button Earn = (Button) findViewById(R.id.button_earn_activity);
 
-        View earnview = findViewById(R.id.button_earnintegralactivity);
+        View view1 = findViewById(R.id.button_earn_activity);
 
-        earn.setOnClickListener(new View.OnClickListener() {
+        Earn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View earnview) {
+            public void onClick(View view1) {
                 Intent intent = new Intent(thisActivity, EarnIntegralActivity.class);
                 thisActivity.startActivity(intent);
-
+                //thisActivity.startActivityForResult(intent, 2016);
             }
         });
 
-        Button exchange = (Button) findViewById(R.id.button_exchangeintegralactivity);
+        Button btnOpen = (Button) findViewById(R.id.button_exchange_activity);
 
-        View exchangeview = findViewById(R.id.button_exchangeintegralactivity);
+        View view2 = findViewById(R.id.button_exchange_activity);
 
-        exchange.setOnClickListener(new View.OnClickListener() {
+        btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View exchangeview) {
+            public void onClick(View view2) {
                 Intent intent = new Intent(thisActivity, ExchangeIntegralActivity.class);
                 thisActivity.startActivity(intent);
-
+                //thisActivity.startActivityForResult(intent, 2016);
             }
         });
-
     }
 }
