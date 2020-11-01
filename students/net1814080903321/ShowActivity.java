919e52532807629093cabe1408f487f251a30028
Classic-Z <951473204@qq.com>
2020-11-01 22:49:22 +0800

    #4 #281 第4次实验

--- students/net1814080903321/ShowActivity.java
+++ students/net1814080903321/ShowActivity.java
@@ -2,7 +2,12 @@ package edu.hzuapp.androidlabs.net1814080903321;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
 
 public class ShowActivity extends AppCompatActivity {
 
@@ -10,5 +15,43 @@ public class ShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ImageButton kaonai = findViewById(R.id.kaonai);
+        kaonai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ShowActivity.this, StoreActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton fan = findViewById(R.id.fan);
+        fan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ShowActivity.this, StoreActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton hanbao = findViewById(R.id.hanbao);
+        hanbao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ShowActivity.this, StoreActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton roujuan = findViewById(R.id.roujuan);
+        roujuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ShowActivity.this, StoreActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
