44d8947a23b06830cb609c78bd16fd5ee74ecca5
wukai230 <1054960199@qq.com>
2020-11-02 22:18:44 +0800

    #3 #43 第3次实验

--- students/sec1814080911230/java/RankActivity.java
+++ students/sec1814080911230/java/RankActivity.java
@@ -2,21 +2,13 @@ package edu.hzuapps.androidlabs.sec1814080911230;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class StartGameActivity extends AppCompatActivity {
-
-
+public class RankActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_game);
-
-
+        setContentView(R.layout.activity_rank);
     }
 }
---
+++ students/sec1814080911230/java/StartGameActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.sec1814080911230;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_game);
+    }
+}
