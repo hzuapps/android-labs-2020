9c5a8d7ef962d371d847d2a077493e0283b74f0e
xxak11234 <ak_cchyhch@qq.com>
2020-11-02 11:42:30 +0800

    #2 #530 第二次实验

---
+++ students/sec1814080911139/VoiceActivity.java
@@ -0,0 +1,13 @@
+package com.example.k00.androidlabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice);
+    }
+}
