69b9f7397a6cc0d4f096fbf861733eb8a184937f
Wumuuu <995150275@qq.com>
2020-11-03 10:14:19 +0800

    第四次实验

--- students/net1814080903218/Net1814080903218Activity.java
+++ students/net1814080903218/Net1814080903218Activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Net1814080903218Activity extends AppCompatActivity {
 
@@ -13,12 +14,20 @@ public class Net1814080903218Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1814080903218);
-        Button button = findViewById(R.id.button);
+        Button button1 = findViewById(R.id.button1);
         final Net1814080903218Activity _this = this;
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(_this, AskForLeave1814080903218Activity.class);
+                _this.startActivity(intent);
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_this, ReasonActivity.class);
                 _this.startActivity(intent);
             }
         });
