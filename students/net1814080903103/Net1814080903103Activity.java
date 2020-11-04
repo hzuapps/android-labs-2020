38b42e8a5fb7b1891e15861cb499e9218744e6de
Lj-xinfei <1337275914@qq.com>
2020-11-04 11:08:53 +0800

    delete

--- students/net1814080903103/Net1814080903103Activity.java
+++
@@ -1,6 +0,0 @@
-package edu.hzuapps.androidlabs.net1814080903103
-
-public class Net1814080903103Activity{
-	public static void main(String[] args){}
-
-}
--- students/net1814080903129/Net1814080903129Activity.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.net1814080903129;
-
-public class Net1814080903129Activity {
-	
-}
