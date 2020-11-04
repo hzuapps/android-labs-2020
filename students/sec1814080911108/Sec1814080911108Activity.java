2096bed32fab59a7b66a892f52a182ca4fbad0a3
Lj-xinfei <1337275914@qq.com>
2020-11-04 11:22:43 +0800

    delete

--- students/sec1814080911108/Sec1814080911108Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.sec1814080911108;
-
-/**
- * Created by lanying on 2020/9/23
- */
-public class Sec1814080911108Activity {
-    
-}
--- students/sec1814080911128/Sec1814080911128Activity.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.sec1814080911128
-
-public class Sec1814080911128Activity {
-
-}
