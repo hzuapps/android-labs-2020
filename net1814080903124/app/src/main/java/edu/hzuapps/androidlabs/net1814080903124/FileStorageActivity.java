171330c43be849a403318dca5b26d3c643f009a1
SJ4real <1398452553@qq.com>
2020-11-07 15:15:46 +0800

    实验报告图片

---
+++ net1814080903124/app/src/main/java/edu/hzuapps/androidlabs/net1814080903124/FileStorageActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903124;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FileStorageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_storage);
+    }
+}
