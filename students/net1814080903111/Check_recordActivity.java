1b84c1c120390543aebb2085003e6dc7154533e7
Liao-King-Sung <1300215141@qq.com>
2020-11-08 16:39:05 +0800

    #4 #24 第四次实验

--- students/net1814080903111/Check_recordActivity.java
+++ students/net1814080903111/Check_recordActivity.java
@@ -1,8 +1,11 @@
 package edu.hzuapps.androidlabs.net1814080903111;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class Check_recordActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Check_recordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_record);
+
+        TextView tv_showdate =(TextView) findViewById(R.id.tv_date);
+
+        Calendar calendar = Calendar.getInstance();
+        tv_showdate.setText(calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1));
     }
 }
