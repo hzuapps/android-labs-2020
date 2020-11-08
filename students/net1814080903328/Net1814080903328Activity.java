753837042e0d2d3f615886187ced0fd267661cc0
Garfield-zheng <1974637297@qq.com>
2020-11-08 20:55:28 +0800

    #4 #263 第四次实验

--- students/net1814080903328/Net1814080903328Activity.java
+++ students/net1814080903328/Net1814080903328Activity.java
@@ -1,14 +1,12 @@
 package edu.hzuapps.androidlabs.net1814080903328;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Net1814080903328Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +15,24 @@ public class Net1814080903328Activity extends AppCompatActivity {
 
         final Net1814080903328Activity thisActivity = this;
 
-        Button button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(thisActivity, RuleActivity.class);
                 startActivity(intent);
+
+            }
+        });
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(thisActivity, SkillsActivity.class);
+                startActivity(intent);
+
             }
         });
     }
