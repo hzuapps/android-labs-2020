799d5e85c0966c6eb2b37bcf3d7b03828228502f
unknown <you@ailcomgit config --global user.email 1282610556@qq.comgit config --global user.name tillsunrisegit add students/net1814080903202/Net1814080903202Activity.javagit commit -m #1>
2020-11-10 15:44:56 +0800

    #4 #71 第4次实验

--- students/net1814080903202/Comment1814080903202Activity.java
+++ students/net1814080903202/Comment1814080903202Activity.java
@@ -2,13 +2,115 @@ package edu.hzuapps.androidlabs.net1814080903202;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-public class Comment1814080903202Activity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Comment1814080903202Activity extends AppCompatActivity implements View.OnClickListener{
+    private Button send;
+    private ImageButton back;
+    private ImageButton scan;
+    private ListView listView;
+    private EditText editcontent;
+    private SimpleAdapter simpleAdapter;
+    private static List<Map<String,Object>> resultlist;
+    private List<CommentBean> list;
+    private int[] head = {R.drawable.head1,R.drawable.head2,R.drawable.head1};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment1814080903202);
+        send = findViewById(R.id.send);
+        back = findViewById(R.id.back);
+        scan = findViewById(R.id.scan);
+        back.setOnClickListener(this);
+        scan.setOnClickListener(this);
+        listView = findViewById(R.id.CommentList);
+        editcontent = findViewById(R.id.content);
+        if(resultlist == null){
+            resultlist = new ArrayList<>();
+            init();
+            for(CommentBean commentBean : list){
+                Map<String,Object> map = new HashMap<>();
+                map.put("name",commentBean.getName());
+                map.put("time",commentBean.getTime());
+                map.put("content",commentBean.getContent());
+                map.put("head",commentBean.getHead());
+                resultlist.add(map);
+            }
+            simpleAdapter = new SimpleAdapter(Comment1814080903202Activity.this,resultlist,
+                    R.layout.comment_listview_item,
+                    new String[]{"name","time","content","head"},new int[]{R.id.name,R.id.time,R.id.content,R.id.head});
+            listView.setAdapter(simpleAdapter);
+        }else{
+            simpleAdapter = new SimpleAdapter(Comment1814080903202Activity.this,resultlist,
+                    R.layout.comment_listview_item,
+                    new String[]{"name","time","content","head"},new int[]{R.id.name,R.id.time,R.id.content,R.id.head});
+            listView.setAdapter(simpleAdapter);
+        }
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = editcontent.getText().toString();
+                if(content.equals("")){
+                    Toast.makeText(Comment1814080903202Activity.this,"发送内容为空！",Toast.LENGTH_LONG).show();
+                }else{
+                    CommentBean commentBean = new CommentBean();
+                    commentBean.setName("Ghost");
+                    Date date = new Date();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                    commentBean.setTime(simpleDateFormat.format(date));
+                    commentBean.setContent(content);
+                    commentBean.setHead(R.drawable.ghost);
+                    list.add(commentBean);
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("name",commentBean.getName());
+                    map.put("time",commentBean.getTime());
+                    map.put("content",commentBean.getContent());
+                    map.put("head",commentBean.getHead());
+                    resultlist.add(map);
+                    simpleAdapter.notifyDataSetChanged();
+                    Toast.makeText(Comment1814080903202Activity.this,"发送成功！",Toast.LENGTH_LONG).show();
+                    editcontent.setText("");
+                }
+            }
+        });
+    }
+    private void init(){
+        list = new ArrayList<>();
+        CommentBean commentBean1 = new CommentBean("Lunette","2020/10/31 12:21:11","今天天气怎么样？",R.drawable.head1);
+        CommentBean commentBean2 = new CommentBean("viices","2020/11/1 10:01:51","有人在学校吗？",R.drawable.head2);
+        CommentBean commentBean3 = new CommentBean("tillsunrise","2020/11/8 9:12:45","有人去吃饭吗？",R.drawable.head3);
+        list.add(commentBean1);
+        list.add(commentBean2);
+        list.add(commentBean3);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        switch (v.getId()) {
+            case R.id.back:
+                onBackPressed();
+                break;
+            case R.id.scan:
+                intent = new Intent(this, Scan1814080903202Activity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
--- students/net1814080903202/Moment1814080903202Activity.java
+++ students/net1814080903202/Moment1814080903202Activity.java
@@ -2,13 +2,128 @@ package edu.hzuapps.androidlabs.net1814080903202;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class Moment1814080903202Activity extends AppCompatActivity {
-
+public class Moment1814080903202Activity extends AppCompatActivity implements View.OnClickListener{
+    private ListView listView;
+    private ImageButton back;
+    private ImageButton scan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moment1814080903202);
+        listView = findViewById(R.id.MomentList);
+        back = findViewById(R.id.back);
+        scan = findViewById(R.id.scan);
+        back.setOnClickListener(this);
+        scan.setOnClickListener(this);
+        listView.setAdapter(new MomentAdapter(this));
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        switch (v.getId()){
+            case R.id.back:
+                onBackPressed();
+                break;
+            case R.id.scan:
+                intent = new Intent(this, Scan1814080903202Activity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
+
+class MomentAdapter extends BaseAdapter{
+    private Context mContext;
+    private LayoutInflater layoutInflater;
+    public MomentAdapter(Context context){
+        mContext = context;
+        layoutInflater = LayoutInflater.from(mContext);
+    }
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+    public static class ViewHolder{
+        public ImageView head;
+        public TextView name;
+        public TextView time;
+        public TextView content;
+        public ImageView img1;
+        public ImageView img2;
+        public ImageView img3;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if(convertView == null){
+            convertView = layoutInflater.inflate(R.layout.moment_listview_item,null);
+            viewHolder = new ViewHolder();
+            viewHolder.head = convertView.findViewById(R.id.head);
+            viewHolder.name = convertView.findViewById(R.id.name);
+            viewHolder.time = convertView.findViewById(R.id.time);
+            viewHolder.content = convertView.findViewById(R.id.content);
+            viewHolder.img1 = convertView.findViewById(R.id.img1);
+            viewHolder.img2 = convertView.findViewById(R.id.img2);
+            viewHolder.img3 = convertView.findViewById(R.id.img3);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.head.setBackgroundResource(heads[position]);
+        viewHolder.name.setText(names[position]);
+        viewHolder.time.setText(times[position]);
+        viewHolder.content.setText(contents[position]);
+        viewHolder.img1.setBackgroundResource(img[0]);
+        viewHolder.img2.setBackgroundResource(img[1]);
+        if (position != 1){
+            viewHolder.img3.setBackgroundResource(img[0]);
+        }
+
+        return convertView;
+    }
+    public Integer[] heads = {
+        R.drawable.head1,
+        R.drawable.head2,
+
+    };
+    public String[] names = {
+            "Lunette",
+            "viices",
+    };
+    public String[] times = {
+            "2小时前",
+            "3小时前",
+    };
+    public String[] contents = {
+            "123",
+            "456"
+    };
+    public Integer[] img = {
+            R.drawable.head1,
+            R.drawable.head2,
+
+    };
 }
