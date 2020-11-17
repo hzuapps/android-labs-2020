fd25a5395b1d7a2c23085d0c2c06ab9a242a06af
Cai-J <yaozizha@outlook.com>
2020-11-17 15:26:38 +0800

    #4 #85 第4次实验

--- students/net1814080903231/java/edu/hzuapps/android/net1814080903231/MainActivity.java
+++ students/net1814080903231/java/edu/hzuapps/android/net1814080903231/MainActivity.java
@@ -7,29 +7,49 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTextView;
+    private Button person_list_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnTextView = findViewById(R.id.btn_textview);
 
-        final Activity thisActivity = this;
+        person_list_button = findViewById(R.id.person_list);
 
-        Button btnOpen = (Button) findViewById(R.id.button);
-
-        View view = findViewById(R.id.button);
-
-        btnOpen.setOnClickListener(new View.OnClickListener() {
-
+        mBtnTextView.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(thisActivity,ChatInterfaceActivity.class);
-                thisActivity.startActivityForResult(intent,2016);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent);
             }
-
         });
 
+        person_list_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ContactPersonActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+//        final Activity thisActivity = this;
+//        Button btnOpen = (Button) findViewById(R.id.button);
+//        View view = findViewById(R.id.button);
+//        btnOpen.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(thisActivity,ChatInterfaceActivity.class);
+//                thisActivity.startActivityForResult(intent,2016);
+//            }
+//        });
     }
 }
