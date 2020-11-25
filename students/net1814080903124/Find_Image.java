a7e96aeb2249f83417c57b381ef1c88448626082
SJ4real <1398452553@qq.com>
2020-11-25 10:16:58 +0800

    #5 #29 第五次实验

---
+++ students/net1814080903124/Find_Image.java
@@ -0,0 +1,42 @@
+package edu.hzuapps.androidlabs.net1814080903124;
+
+public class Find_Image {
+    public static Integer[] Sneaker_Image = {
+            R.drawable.shoes_01,
+            R.drawable.shoes_02,
+            R.drawable.shoes_03,
+            R.drawable.shoes_04,
+            R.drawable.shoes_05,
+            R.drawable.shoes_06,
+            R.drawable.shoes_07,
+            R.drawable.shoes_08,
+            R.drawable.shoes_09,
+            R.drawable.shoes_05,
+            R.drawable.shoes_06,
+            R.drawable.shoes_07,
+            R.drawable.shoes_08,
+            R.drawable.shoes_09,
+            R.drawable.shoes_03,
+            R.drawable.shoes_04,
+            R.drawable.shoes_01
+    };
+
+    public static Integer[] Clothes_Image = {
+            R.drawable.clothes_01,
+            R.drawable.clothes_02,
+            R.drawable.clothes_03,
+            R.drawable.clothes_01,
+            R.drawable.clothes_02,
+            R.drawable.clothes_03
+    };
+
+    public static Integer[] ACC_Image = {
+            R.drawable.acc_01,
+            R.drawable.acc_02,
+            R.drawable.acc_03,
+            R.drawable.acc_01,
+            R.drawable.acc_02,
+            R.drawable.acc_03
+    };
+
+}
