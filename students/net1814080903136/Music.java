9723f3fb9277aa09ced7ee2f9081e13ec60b91af
yesijie0216 <1306187160@qq.com>
2020-11-26 21:37:52 +0800

    #5 #148 第五次实验

--- students/net1814080903136/Music.java
+++ students/net1814080903136/Music.java
@@ -1,6 +1,6 @@
 package edu.hzuapps.androidlabs.net1814080903136;
 
-public class music {
+public class Music {
     public String song;//歌曲名
     public String singer;//歌手
     public long size;//歌曲所占空间大小
