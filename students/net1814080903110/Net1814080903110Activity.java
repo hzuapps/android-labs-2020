36790886c6ace182e6adf52ceac36d65c4b3d441
xyl123580 <869602730@qq.com>
2020-11-27 22:33:49 +0800

    #5 #26 第五次实验

--- students/net1814080903110/Net1814080903110Activity.java
+++ students/net1814080903110/Net1814080903110Activity.java
@@ -1,30 +1,43 @@
 package edu.hzuapps.androidlabs.Net1814080903110;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.content.Intent;
-import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class Net1814080903110Activity extends AppCompatActivity {
 
+    private TestCourseData mySQLiteOpenHelper;
+    private String schedule;
+    private SQLiteDatabase myDatabase;
+    private Context context;
+    private TextView mySchedule[] = new TextView[5];
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1814080903110);
 
+        //跳转按钮
         Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {
-                                          Intent intent = new Intent(Net1814080903110Activity.this, SettingActivity.class);
-                                          startActivity(intent);
-                                      }
-                                  });
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Net1814080903110Activity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
         Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,28 +46,7 @@ public class Net1814080903110Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        ListView lv = (ListView) findViewById(R.id.dates);
-
-        ArrayAdapter s1 = new ArrayAdapter<String>(this, R.layout.listview_item, mobileArray);
-
-        lv.setAdapter(s1);
-
-
-        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        };
-        lv.setOnItemClickListener(listener);
-    }
-
-    String[] mobileArray = { //
-            "日程一", "日程二", "日程三", "日程四"
-    };
-
-}
+    }}
 
 
 
