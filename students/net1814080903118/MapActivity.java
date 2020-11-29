d39c59b5f22d47dc32dc4b2f3964f34ddd6438ec
GBSTS <3350861305@qq.com>
2020-11-29 19:37:20 +0800

    #6 #49 第六次实验

---
+++ students/net1814080903118/MapActivity.java
@@ -0,0 +1,38 @@
+package edu.hzuapps.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.map.MapView;
+
+public class MapActivity extends Activity {
+
+    private MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+}
