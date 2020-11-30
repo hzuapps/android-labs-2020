aa83e8a72639993aa47b910be557b22b491253f2
xyl123580 <869602730@qq.com>
2020-11-30 08:19:18 +0800

    #6 #26 第六次实验

--- students/net1814080903110/Net1814080903110Activity.java
+++ students/net1814080903110/Net1814080903110Activity.java
@@ -2,7 +2,6 @@ package edu.hzuapps.androidlabs.net;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
