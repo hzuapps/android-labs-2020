3cac42dac37cc4b7bdaf1a9f6946841b531c5529
zhengtingting1 <1871464353@qq.com>
2020-12-02 19:53:55 +0800

    #2 #565 第2次实验

---
+++ students/net1814080903241/app/src/main/java/edu/hzuapps/androidlabs/net181408903241/MarkActivity.java
@@ -0,0 +1,10 @@
+package edu.hzuapps.androidlabs.net181408903241;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class MarkActivity extends  AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_punchtheclock);
+
+    }
+}
