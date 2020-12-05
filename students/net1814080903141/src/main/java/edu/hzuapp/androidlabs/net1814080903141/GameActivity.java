eece7b9ac12c232f3875d03cea5d8277a3a415ee
unknown <1481923078@qq.com>
2020-12-05 17:44:58 +0800

    #5 #421 第5次实验

--- students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/GameActivity.java
+++ students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/GameActivity.java
@@ -1,14 +1,64 @@
 package edu.hzuapp.androidlabs.net1814080903141;
 
+
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
 
+    private Button btnsave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        btnsave=findViewById(R.id.btnsave);
+
+        final GameActivity thisActivity=this;
+        // 保存记录信息
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thisActivity.saveRecord();
+            }
+        });
+
     }
+
+    private void saveRecord() {
+        String score = ((EditText) findViewById(R.id.tscore)).getText().toString();
+        String best = ((EditText) findViewById(R.id.tbest)).getText().toString();
+
+        // 插入新记录
+        ContentValues record = new ContentValues();
+        //book.put("name", name); // Map <- Key:Value
+        record.put(ContentProviderOfRecords.SCORE,score);
+        record.put(ContentProviderOfRecords.BEST, best);
+
+        Uri uri = getContentResolver() // 执行插入操作
+                .insert(ContentProviderOfRecords.CONTENT_URI, record);
+
+        Toast.makeText(getBaseContext(), //
+                "保存成功! \n" + uri.toString(), Toast.LENGTH_LONG).show();
+
+        this.showRecordInfo("", ""); // 清除内容
+    }
+
+    private void showRecordInfo(String score, String best) {
+        ((EditText) findViewById(R.id.tscore)).setText(score);
+        ((EditText) findViewById(R.id.tbest)).setText(best);
+    }
+
+
 }
+
+
--- students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/RecordActivity.java
+++ students/net1814080903141/src/main/java/edu/hzuapp/androidlabs/net1814080903141/RecordActivity.java
@@ -1,13 +1,73 @@
 package edu.hzuapp.androidlabs.net1814080903141;
 
+import android.content.ContentValues;
+
+
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecordActivity extends AppCompatActivity {
+
+    private Button btnclear;
+    private Button btncheck;
+    private StringBuilder sb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        btncheck = findViewById(R.id.btncheak);
+        btnclear=findViewById(R.id.btnclear);
+
+        final RecordActivity thisActivity = this;
+
+
+        btncheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisActivity.showRecord();
+            }
+        });
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisActivity.clearRecord();
+            }
+        });
     }
+    private void showRecord() {
+        sb =new  StringBuilder();
+        // 插入新记录
+        Cursor cursor=getContentResolver().query(ContentProviderOfRecords.CONTENT_URI,null,null,null,null);
+
+        if (cursor.moveToFirst()){
+            do {
+                int gscore=cursor.getInt(cursor.getColumnIndex(ContentProviderOfRecords.SCORE));
+                int gbest=cursor.getInt(cursor.getColumnIndex(ContentProviderOfRecords.BEST));
+                sb.append("Score:"+gscore+"     "+"Best:"+gbest+"\n");
+                ((TextView) findViewById(R.id.trecord)).setText(sb);
+            }while ((cursor.moveToNext()));
+
+        }
+        cursor.close();
+    }
+    private void clearRecord(){
+        getContentResolver().delete(ContentProviderOfRecords.CONTENT_URI,null,null);
+        Toast.makeText(getBaseContext(), //
+                "删除成功! \n" + sb.toString(), Toast.LENGTH_LONG).show();
+        ((TextView) findViewById(R.id.trecord)).setText("");
+
+    }
+
+
 }
