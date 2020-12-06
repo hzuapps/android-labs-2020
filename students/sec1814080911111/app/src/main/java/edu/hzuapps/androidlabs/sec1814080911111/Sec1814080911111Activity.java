31d2c74d6ef503355a6a2eb5c80c44d751487add
holenchan <1370227996@qq.com>
2020-12-06 21:11:04 +0800

    #5 #277 第5次实验

--- students/sec1814080911111/app/src/main/java/edu/hzuapps/androidlabs/sec1814080911111/Sec1814080911111Activity.java
+++ students/sec1814080911111/app/src/main/java/edu/hzuapps/androidlabs/sec1814080911111/Sec1814080911111Activity.java
@@ -1,19 +1,24 @@
 package edu.hzuapps.androidlabs.sec1814080911111;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import helper.FileHelper;
 
 public class Sec1814080911111Activity extends AppCompatActivity {
 
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button timer = (Button)findViewById(R.id.button4);
+        Button timer = (Button)findViewById(R.id.timer);
         timer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,13 +27,26 @@ public class Sec1814080911111Activity extends AppCompatActivity {
             }
         });
 
-        Button countDown = (Button)findViewById(R.id.button5);
+        Button countDown = (Button)findViewById(R.id.countDown);
         countDown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Sec1814080911111Activity.this,TimerActivity.class);
+                Intent intent = new Intent(Sec1814080911111Activity.this,CountdownActivity.class);
                 startActivity(intent);
             }
         });
+
+        mContext = getApplicationContext();
+        FileHelper fHelper = new FileHelper(mContext);
+        String fileName = "timer";
+        String fileContent = "timer init";
+
+        try {
+            fHelper.save(fileName, fileContent);
+            Toast.makeText(getApplicationContext(), "欢迎使用", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "欢迎使用", Toast.LENGTH_SHORT).show();
+        }
     }
 }
