fd3829a6b7234b1dda40d009ac6b621dbcd49e13
18cs06 <1449767333@qq.com.com>
2020-12-06 19:37:20 +0800

    Update FileHelper.java

--- students/sec1814080911106/FileHelper.java
+++ students/sec1814080911106/FileHelper.java
@@ -18,31 +18,27 @@ public class FileHelper {
         this.mContext = mContext;
     }
 
-    /*
-     * 这里定义的是一个文件保存的方法，写入到文件中，所以是输出流
-     * */
+
     public void save(String filename, String filecontent) throws Exception {
-        //这里我们使用私有模式,创建出来的文件只能被本应用访问,还会覆盖原文件哦
+
         FileOutputStream output = mContext.openFileOutput(filename, Context.MODE_PRIVATE);
-        output.write(filecontent.getBytes());  //将String字符串以字节流的形式写入到输出流中
-        output.close();         //关闭输出流
+        output.write(filecontent.getBytes());
+        output.close();
     }
 
 
-    /*
-     * 这里定义的是文件读取的方法
-     * */
+
     public String read(String filename) throws IOException {
-        //打开文件输入流
+
         FileInputStream input = mContext.openFileInput(filename);
         byte[] temp = new byte[1024];
         StringBuilder sb = new StringBuilder("");
         int len = 0;
-        //读取文件内容:
+
         while ((len = input.read(temp)) > 0) {
             sb.append(new String(temp, 0, len));
         }
-        //关闭输入流
+
         input.close();
         return sb.toString();
     }
