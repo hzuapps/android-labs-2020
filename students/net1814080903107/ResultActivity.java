aa87cc7905faacd8b1925fada747b45211b390e5
lyRomantic <2814757819@qq.com>
2020-12-15 20:19:22 +0800

    #6 #27 第六次实验

---
+++ students/net1814080903107/ResultActivity.java
@@ -0,0 +1,88 @@
+package com.example.thefirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class ResultActivity extends AppCompatActivity {
+    private MapView mMapView = null;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+        //初始化地图控制器对象
+        AMap aMap=mMapView.getMap();
+        aMap.setTrafficEnabled(true);// 显示实时交通状况
+        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+        aMap.setMapType(AMap.MAP_TYPE_NORMAL);
+
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        /**
+         * 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+         */
+        mLocationOption.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+}
--- students/net1814080903107/StoreActivity.java
+++ students/net1814080903107/StoreActivity.java
@@ -5,32 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.example.thefirst.component.MyFoodLinearLayout;
+
+import java.net.IDN;
 import java.net.Inet4Address;
 
 public class StoreActivity extends AppCompatActivity {
-    boolean flag=false;
-    ListView listView;
-    String storeName;
+    int startPoint = 0;
+    int startPoint1 = 1;
+    int startPoint2 = 2;
+    private boolean flag=false;
+    private ListView listView;
+    private TextView storeNameView;
+    private int storeID;
+    private ScrollView mainView;
+    private ScrollView sideView;
+    private ScrollView snackView;
+    private ScrollView shopCar;
+    private LinearLayout mainViewS;
+    private LinearLayout sideViewS;
+    private LinearLayout snackViewS;
+    private LinearLayout shopCarS;
+    private Button phoneButton;
+    private TextView storeIntroduction;
+    private ImageView storeBitmap;
     @Override
-    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
-        Intent i=getIntent();
+        Intent intent=getIntent();
         listView=findViewById(R.id.chooseList);
-        //set store name
-        TextView textView=findViewById(R.id.storeName);
-        textView.setText(i.getStringExtra("title"));
-        storeName=i.getStringExtra("id");
+        storeNameView=findViewById(R.id.storeName);
+        mainView=findViewById(R.id.mainView);
+        sideView=findViewById(R.id.sideView);
+        snackView=findViewById(R.id.snackView);
+        shopCar=findViewById(R.id.shopCar);
+        mainViewS=findViewById(R.id.mainViewS);
+        sideViewS=findViewById(R.id.sideViewS);
+        snackViewS=findViewById(R.id.snackViewS);
+        shopCarS=findViewById(R.id.shopCarS);
+
+        phoneButton=findViewById(R.id.phoneButton);
+        storeIntroduction=findViewById(R.id.storeIntroduction);
+        storeBitmap=findViewById(R.id.storeBitmap);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @SuppressLint("WrongConstant")
@@ -39,24 +72,138 @@ public class StoreActivity extends AppCompatActivity {
                 View v=null;
                 switch (position){
                     case 0:
-                        v=findViewById(R.id.mainView);
+                        v=mainView;
                         break;
                     case 1:
-                        v=findViewById(R.id.sideView);
+                        v=sideView;
                         break;
                     case 2:
-                        v=findViewById(R.id.snackView);
+                        v=snackView;
                         break;
+                    case 3:
+                        v=shopCar;
                 }
                 //置顶
-                v.bringToFront();
+                mainView.setVisibility(View.INVISIBLE);
+                sideView.setVisibility(View.INVISIBLE);
+                snackView.setVisibility(View.INVISIBLE);
+                shopCar.setVisibility(View.INVISIBLE);
+                v.setVisibility(View.VISIBLE);
             }
         });
+        init(intent);
+        initListen();
+        initShowCarS();
+        //初始化商品
+        new LoadFoodThread(this,mainViewS, storeID,0,shopCarS).start();
+        new LoadFoodThread(this,sideViewS, storeID,1,shopCarS).start();
+        new LoadFoodThread(this,snackViewS, storeID,2,shopCarS).start();
     }
-    public void click(View v){
-        Intent it=new Intent();
-        it.setType("video/*");
-        startActivityForResult(it,101);
+    //初始化购物车
+    private void initShowCarS(){
+        LinearLayout linearLayout=new LinearLayout(shopCarS.getContext());
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        Button buy=new Button(linearLayout.getContext());
+        buy.setText("购买");
+        Button clear=new Button(linearLayout.getContext());
+        clear.setText("清空");
+        //跳转页面
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newPage=new Intent(StoreActivity.this,ResultActivity.class);
+                StoreActivity.this.startActivity(newPage);
+            }
+        });
+        //清除
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StoreActivity.this.shopCarS.removeAllViews();
+                initShowCarS();
+            }
+        });
+        linearLayout.addView(buy);
+        linearLayout.addView(clear);
+        shopCarS.addView(linearLayout);
+    }
+    //滑动监听
+    @SuppressLint("ClickableViewAccessibility")
+    private void initListen(){
+
+        mainView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN :
+                        break;
+                    case MotionEvent.ACTION_MOVE :
+                        break;
+                    case MotionEvent.ACTION_UP :
+                        if(mainView.getChildAt(0).getMeasuredHeight()<=mainView.getScrollY()+mainView.getHeight()){
+                            new LoadFoodThread(StoreActivity.this,mainViewS,storeID,0,shopCarS).start();
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+
+        sideView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN :
+                        break;
+                    case MotionEvent.ACTION_MOVE :
+                        break;
+                    case MotionEvent.ACTION_UP :
+                        if(sideView.getChildAt(0).getMeasuredHeight()<=sideView.getScrollY()+sideView.getHeight()){
+                            new LoadFoodThread(StoreActivity.this,sideViewS,storeID,1,shopCarS).start();
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+
+        snackView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN :
+                        break;
+                    case MotionEvent.ACTION_MOVE :
+                        break;
+                    case MotionEvent.ACTION_UP :
+                        if(snackView.getChildAt(0).getMeasuredHeight()<=snackView.getScrollY()+snackView.getHeight()){
+                            new LoadFoodThread(StoreActivity.this,mainViewS,storeID,2,shopCarS).start();
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+    private void init(final Intent intent){
+        storeID=intent.getIntExtra("storeID",0);
+        storeNameView.setText(intent.getStringExtra("storeName"));
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("tel:"+intent.getStringExtra("phone")));
+                StoreActivity.this.startActivity(i);
+            }
+        });
+        //address
+        storeIntroduction.setText(intent.getStringExtra("introduction"));
+        Bundle bdl=intent.getExtras();
+        Bitmap bitmap=(Bitmap)bdl.get("storeBitmap");
+        storeBitmap.setImageBitmap(bitmap);
     }
     protected void onActivityResult(int reqCode,int resCode,Intent data) {
         super.onActivityResult(reqCode, resCode, data);
