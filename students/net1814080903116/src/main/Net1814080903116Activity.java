476ef4f5191ad5afc19c11e1d4d9de3df8f84e92
Dlmdp <770452043@qq.com>
2020-12-18 13:54:35 +0800

    #7 #122 第七次实验报告

--- students/net1814080903116/src/main/Net1814080903116Activity.java
+++ students/net1814080903116/src/main/Net1814080903116Activity.java
@@ -9,12 +9,22 @@ import android.widget.Button;
 
 public class Net1814080903116Activity extends AppCompatActivity {
 
+
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindActivityButton(R.id.postButton, PostActivity.class);
         bindActivityButton(R.id.treeHoldButton, TreeholdActivity.class);
+
+        // 启动服务播放背景音乐
+        intent = new Intent(Net1814080903116Activity.this, MyService.class);
+        String action = MyService.ACTION_MUSIC;
+        // 设置action
+        intent.setAction(action);
+        startService(intent);
+
 
     }
 
@@ -31,5 +41,14 @@ public class Net1814080903116Activity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (intent != null){
+            // 对于intentService，这一步可能是多余的
+            stopService(intent);
+        }
     }
 }
