f1c31f0544691e1351ceea6d8552c07b1f9e0822
BlaiseChin <13059365076@163.com>
2020-12-20 22:11:34 +0800

    #5 #37 第5次实验

---
+++ students/sec1814080911113/text5/LocalMusicBean.java
@@ -0,0 +1,89 @@
+package edu.hzuapps.androidlabs.sec1814080911113;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class LocalMusicBean {
+
+    private String id; //歌曲id
+    private String song; //歌曲名称
+    private String singer; //歌手名称
+    private String album; //专辑名称
+    private String duration; //歌曲时长
+    private String path; //歌曲路径
+    private String albumArt;  //专辑地址
+
+    public LocalMusicBean() {
+    }
+
+    public LocalMusicBean(String id, String song, String singer, String album, String duration, String path, String albumArt) {
+        this.id = id;
+        this.song = song;
+        this.singer = singer;
+        this.album = album;
+        this.duration = duration;
+        this.path = path;
+        this.albumArt = albumArt;
+    }
+
+    public String getAlbumArt() {
+        return albumArt;
+    }
+
+    public void setAlbumArt(String albumArt) {
+        this.albumArt = albumArt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
