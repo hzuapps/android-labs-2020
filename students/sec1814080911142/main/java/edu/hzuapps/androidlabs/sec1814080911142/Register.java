a10b6af1b4aed9da4a47e4e410f560c6576abc5d
lxp99 <1604191933@qq.com>
2020-12-21 12:44:37 +0800

    newbranch

---
+++ students/sec1814080911142/main/java/edu/hzuapps/androidlabs/sec1814080911142/Register.java
@@ -0,0 +1,33 @@
+package edu.hzuapps.androidlabs.sec1814080911142;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final Activity thisActivity = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.smdl);
+        final Button button1 = (Button) findViewById(R.id.button11);
+        final Button button2 = (Button) findViewById(R.id.button22);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity, sec1814080911142Activity.class);
+                thisActivity.startActivity(intent);
+                thisActivity.finish();
+
+
+            }
+
+
+
+        });
+    }
+
+
+}
