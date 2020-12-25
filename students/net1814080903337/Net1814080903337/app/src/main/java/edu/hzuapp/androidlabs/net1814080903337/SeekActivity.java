4c369ef8dcd7e0f911b0b712b40b4a715f57566c
unknown <1953757088@qq.com>
2020-12-25 20:50:49 +0800

    #5 #248 第5次实验

--- students/net1814080903337/Net1814080903337/app/src/main/java/edu/hzuapp/androidlabs/net1814080903337/SeekActivity.java
+++ students/net1814080903337/Net1814080903337/app/src/main/java/edu/hzuapp/androidlabs/net1814080903337/SeekActivity.java
@@ -2,13 +2,29 @@ package edu.hzuapp.androidlabs.net1814080903337;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SeekActivity extends AppCompatActivity {
-
+public class SeekActivity extends Activity {
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seek);
+        button=(Button)findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                finish();
+                Intent intent = new Intent(SeekActivity.this, SeekResult.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
