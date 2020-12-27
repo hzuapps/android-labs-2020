dbcb6be21c9fe05efa0c7fd17d9c5cd512b31cf7
Classic-Z <951473204@qq.com>
2020-12-27 16:55:40 +0800

    删除不必要的文件

--- students/Net1814080903223/Net1814080903223Activity.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.net1814080903211;
-
-public class Net1814080903211Activity {
-    static public void main(String[] args) {
-        System.out.println("holle world.");
-    }
-}
