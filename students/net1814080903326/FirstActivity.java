0cbe777714f667b9bb19a9e143c4d6aa1d49e80d
zxcvbn135 <1146241916@qq.com>
2020-12-31 15:32:32 +0800

    #4 #280 第四次实验

---
+++ students/net1814080903326/FirstActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.net1814080903326;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
--- students/net1814080903326/Net1814080903326Activity.java
+++ students/net1814080903326/Net1814080903326Activity.java
@@ -1,34 +1,41 @@
-package hzu.android.labs;
+package edu.hzuapps.androidlabs.net1814080903326;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class Net1814080903326Activity extends AppCompatActivity {
+public class Net1814080903326Activity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_net1814080903326);
+
+        initUI();
+    }
+
+    private void initUI() {
+        findViewById(R.id.btn_inf).setOnClickListener(this);
+        findViewById(R.id.btn_mine).setOnClickListener(this);
+    }
 
 
-        final Net1814080903326Activity thisActivity = this;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_inf:
+                //跳转到第二界面
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(),FirstActivity.class);
+                this.startActivity(intent);
+                break;
+            case R.id.btn_mine:
+                //跳转到第二界面
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(thisActivity, PageTurning1.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
+                break;
+        }
 
     }
 }
