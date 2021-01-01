1201e9ec386f4e031c344668bb064c5162c390ed
zcjzzf <2268303279@qq.com>
2021-01-01 21:39:26 +0800

    #4 # 407 第四次实验

--- students/net1814080903214/app/src/main/java/edu/hzuapps/androidlabs/StartActivity.java
+++ students/net1814080903214/app/src/main/java/edu/hzuapps/androidlabs/StartActivity.java
@@ -1,14 +1,28 @@
-package edu.hzuapp.androidlabs;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_game);
+
+        Button toHistoryScore = (Button)findViewById(R.id.toHistoryScore);
+        toHistoryScore.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View v) {
+                                                  Intent intent = new Intent();
+                                                  intent.setClass(GameActivity.this, HistoryScoreActivity.class);
+                                                  startActivity(intent);
+                                              }
+                                          }
+        );
+
+
     }
 }
