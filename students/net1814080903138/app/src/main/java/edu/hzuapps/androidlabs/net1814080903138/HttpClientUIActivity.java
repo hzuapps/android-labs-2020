61ba5c94c167ff350e628bf7ab0b834475f4d404
linjhong <2283335452@qq.com>
2021-01-03 20:37:59 +0800

    #5 #156 第五次实验结果截图

---
+++ students/net1814080903138/app/src/main/java/edu/hzuapps/androidlabs/net1814080903138/HttpClientUIActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.net1814080903138;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HttpClientUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_client_u_i);
+    }
+}
