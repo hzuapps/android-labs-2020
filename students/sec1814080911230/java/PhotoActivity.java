6c7d9511fe4cecd4e0b7ce40be45ac940bd7fb45
wukai230 <1054960199@qq.com>
2021-01-04 10:01:51 +0800

    #7 #43 第7次实验

---
+++ students/sec1814080911230/java/PhotoActivity.java
@@ -0,0 +1,59 @@
+package edu.hzuapps.androidlabs.sec1814080911230;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class PhotoActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+
+        // 准备界面
+        linearLayout = findViewById(R.id.linear_layout);
+
+        // 处理点击按钮
+        ((Button) findViewById(R.id.button_take_photo)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // 请求拍照
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    // 请求拍照
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    // 获取缩略图
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = new ImageView(this);
+            // TODO: 如何设置图片的大小？
+            imageView.setImageBitmap(imageBitmap);
+            linearLayout.addView(imageView);
+        }
+    }
+}
