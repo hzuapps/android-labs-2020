ec3607527bc56ded8da16319eeac6d8f0fea489b
unknown <1587866643@qq.com>
2021-01-04 18:31:56 +0800

    #5 #79 第五次实验

---
+++ students/net1814080903312/app/src/main/java/edu/hzuapps/androidlabs/net1814080903312/MusicActivity.java
@@ -0,0 +1,19 @@
+package edu.hzuapps.androidlabs.net1814080903312;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.media.MediaPlayer;
+
+public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+
+}
