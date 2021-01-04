d1368b00f3f02dbea365d6c1125b3a1e8c9ceea7
JieBro333 <JieBro333.com>
2021-01-04 16:39:23 +0800

    #7 #81 第7次实验

--- students/net1814080903220/Net1814080903220MainActivity.java
+++ students/net1814080903220/Net1814080903220MainActivity.java
@@ -1,37 +1,56 @@
-package edu.hzuapps.androidlabs.Net1814080903220;
+package edu.hzuapps.androidlabs.net1814080903220;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class Net1814080903220MainActivity extends AppCompatActivity {
-
-    private Button Edit;
-    private Button Picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
-        Edit = (Button) findViewById(R.id.E_dit);
-        Edit.setOnClickListener(new View.OnClickListener() {
+
+        Button edit = (Button) findViewById(R.id.E_dit);
+        edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Net1814080903220MainActivity.this,Net1814080903220EditActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(Net1814080903220MainActivity.this, Net1814080903220EditActivity.class);
+                startActivityForResult(intent, 0);
             }
         });
-        Picture = (Button) findViewById(R.id.P_icture);
-        Picture.setOnClickListener(new View.OnClickListener() {
+
+        Button picture = (Button) findViewById(R.id.P_icture);
+        picture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(Net1814080903220MainActivity.this,Net1814080903220PictureActivity.class);
+                intent.setClass(Net1814080903220MainActivity.this, Net1814080903220PictureActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
-        };
+
+        TextView textView = (TextView) findViewById(R.id.content);
+        TextView textView1 = (TextView) findViewById(R.id.apartdate1);
+
     }
+
+    /*@Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("data",textView.getText().toString());
+        outState.putString("data1",textView1.getText().toString());
+    }
+    protected void  onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        String s = savedInstanceState.getString("data");
+        String s1 = savedInstanceState.getString("data1");
+        textView.setText(s);
+        textView.setText(s1);
+    }*/
+};
