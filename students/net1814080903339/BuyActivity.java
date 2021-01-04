561bbc71b4158080c199dc47d75bc1fa73537d5e
39-chen <2816939879@qq.com>
2021-01-04 17:47:03 +0800

    #5 #257 第5次实验

--- students/net1814080903339/BuyActivity.java
+++ students/net1814080903339/BuyActivity.java
@@ -1,14 +1,93 @@
 package edu.hzuapps.androidlabs.net1814080903339activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.Random;
+import edu.hzuapps.androidlabs.net1814080903339activity.R;
 public class BuyActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
+        final BuyActivity thisActivity = this;
+// 保存图书信息
+        findViewById(R.id.button_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thisActivity.saveBook();
+            }
+        });
+// 加载下一本书
+        findViewById(R.id.button_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thisActivity.showNextBook();
+            }
+        });
+    }
+    // 保存图书信息
+    private void saveBook() {
+        String name = ((EditText) findViewById(R.id.book_name)).getText().toString();
+        String tags = ((EditText) findViewById(R.id.book_tags)).getText().toString();
+// 插入新记录
+        ContentValues book = new ContentValues();
+//book.put("name", name); // Map <- Key:Value
+        book.put(ContentProviderOfBooks.NAME, name);
+        book.put(ContentProviderOfBooks.TAGS, tags);
+        Uri uri = getContentResolver() // 执行插入操作
+                .insert(ContentProviderOfBooks.CONTENT_URI, book);
+        Toast.makeText(getBaseContext(), //
+                "保存成功! \n" + uri.toString(), Toast.LENGTH_LONG).show();
+        this.showBookInfo("", ""); // 清除内容
+    }
+    private void showBookInfo(String name, String tags) {
+        ((EditText) findViewById(R.id.book_name)).setText(name);
+        ((EditText) findViewById(R.id.book_tags)).setText(tags);
+    }
+    // 显示下一本书
+    private void showNextBook() {
+//String URL = ContentProviderOfBooks.URL;
+        Cursor c = getContentResolver() // 执行查询
+                .query(ContentProviderOfBooks.CONTENT_URI, null, null, null, null);
+// 随机取1条记录
+        int random = (new Random()).nextInt(10);
+        if (c.moveToFirst()) {
+            String name = "";
+            String tags = "";
+            int count = 1;
+            do {
+                name = c.getString(c.getColumnIndex(ContentProviderOfBooks.NAME));
+                tags = c.getString(c.getColumnIndex(ContentProviderOfBooks.TAGS));
+                if (count++ == random) {
+                    break;
+                }
+            } while (c.moveToNext());
+// 显示在界面上
+            this.showBookInfo(name, tags);
+        } else {
+            Toast.makeText(getBaseContext(), //
+                    "没有可显示的图书!", Toast.LENGTH_LONG).show();
+        }
     }
 }
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//
+//public class BuyActivity extends AppCompatActivity {
+//    String[] a={"四级真题","六级真题","六级听力","四级听力","四级阅读","六级阅读"};
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_buy);
+//        ListView listView =(ListView)findViewById(R.id.mobile_list);
+//        ArrayAdapter arrayAdapter = new ArrayAdapter(this,R.layout.activity_buy_item,a);
+//        listView.setAdapter(arrayAdapter);
+//    }
+//}
---
+++ students/net1814080903339/ContentProviderOfBooks.java
@@ -0,0 +1,154 @@
+package edu.hzuapps.androidlabs.net1814080903339activity;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+import java.util.HashMap;
+/**
+ * 类名最好改为: BookProvider
+ */
+public class ContentProviderOfBooks extends ContentProvider {
+    static final String PROVIDER_NAME = "edu.hzuapps.androidlabs.Library";
+    static final String URL = "content://" + PROVIDER_NAME + "/books";
+    static final Uri CONTENT_URI = Uri.parse(URL);
+    static final String _ID = "_id";
+    static final String NAME = "name";
+    static final String TAGS = "tags";
+    static final int BOOKS = 1;
+    static final int BOOK_ID = 2;
+    static final UriMatcher uriMatcher;
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "books", BOOKS);
+        uriMatcher.addURI(PROVIDER_NAME, "books/#", BOOK_ID);
+    }
+    // 数据库相关操作
+    private SQLiteDatabase db;
+    static final String DATABASE_NAME = "Library";
+    static final String TABLE_BOOKS = "books";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_DB_TABLE =
+            " CREATE TABLE " + TABLE_BOOKS +
+                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " name TEXT NOT NULL, " +
+                    " tags TEXT NOT NULL);";
+    /**
+     * 用于创建数据库的帮助类
+     */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DB_TABLE);
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_BOOKS);
+            onCreate(db);
+        }
+    }
+    private static HashMap<String, String> BOOKS_PROJECTION_MAP;
+    public ContentProviderOfBooks() {
+    }
+    @Override
+    public String getType(Uri uri) {switch (uriMatcher.match(uri)){
+        case BOOKS:
+            return "vnd.android.cursor.dir/vnd.example.students";
+        case BOOK_ID:
+            return "vnd.android.cursor.item/vnd.example.students";
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+    }
+    }
+    // 插入|保存一条记录
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long rowID = db.insert(TABLE_BOOKS, "", values);
+        if (rowID > 0) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        throw new SQLException("无法插入数据 " + uri);
+    }
+    // 初始化内容提供器
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+// 创建可写的数据库（如果没有则新建）
+        db = dbHelper.getWritableDatabase();
+//db = dbHelper.getReadableDatabase()
+        return (db == null) ? false : true;
+    }
+    // 查询记录
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(TABLE_BOOKS);
+        switch (uriMatcher.match(uri)) {
+            case BOOKS:
+                qb.setProjectionMap(BOOKS_PROJECTION_MAP);
+                break;
+            case BOOK_ID:
+                qb.appendWhere(_ID + "=" + uri.getPathSegments().get(1));
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        if (sortOrder == null || sortOrder == "") {
+            sortOrder = NAME;
+        }
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case BOOKS:
+                count = db.update(TABLE_BOOKS, values, selection, selectionArgs);
+                break;
+            case BOOK_ID:
+                count = db.update(TABLE_BOOKS, values, _ID + " = " + uri.getPathSegments().get(1) +
+                        (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case BOOKS:
+                count = db.delete(TABLE_BOOKS, selection, selectionArgs);
+                break;
+            case BOOK_ID:
+                String id = uri.getPathSegments().get(1);
+                count = db.delete(TABLE_BOOKS, _ID + " = " + id +
+                        (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+}
