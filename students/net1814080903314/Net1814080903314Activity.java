1d0f0047ab7ad8b75d81720ae17d0cb377a16669
Aya <AzStartie@qq.com>
2021-01-05 16:58:49 +0800

    #5 #266 第五次实实验

--- students/net1814080903314/Net1814080903314Activity.java
+++ students/net1814080903314/Net1814080903314Activity.java
@@ -5,9 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Net1814080903314Activity extends AppCompatActivity {
-private Button button;
+    private Button button;
+    private Button Icon;
+    private int[] mImage;
+    private TextView mText;
+    private int num;
+    private int index;
+    private String[] title;
+    private int[] images;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,20 @@ private Button button;
             startActivity(intent);
     }
 });
+        Icon = (Button) findViewById(R.id.icn_icon);
+        Icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iconIntent = new Intent(Net1814080903314Activity.this , IconActivity.class);
+                startActivity(iconIntent);
+            }
+        });
+    }
+    public void initData() {
+        title = new String[]{"截图1", "截图2"};
+        mImage = new int[]{R.drawable.phot, R.drawable.sirr};
+    }
+    public void updateImageAndTitle() {
+        mText.setText(title[index]);
     }
 }
