850147cac9dbcf52814c72198dc6b83f0610ef8c
forever-young-hh <1198691257@qq.com>
2021-01-06 19:17:59 +0800

    #5 #245 第五次实验

--- students/net1814080903335/ChoseActivity.java
+++ students/net1814080903335/ChoseActivity.java
@@ -7,21 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Net1814080903335Activity extends AppCompatActivity {
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class ChoseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose);
         setContentView(R.layout.activity_main);
 
-        final Net1814080903335Activity thisActivity = this;
+        final ChoseActivity thisActivity = this;
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(thisActivity, ChoseActivity.class);
+                Intent intent = new Intent(thisActivity, ReadActivity.class);
                 startActivity(intent);
             }
         });
