c88547a421a5c7a1a81f419e6adf0fd1bc28b096
“You <429477910@qq.com>
2021-01-07 14:26:02 +0800

    修改第4次实验

--- students/net1814080903342/SearchIdActivity.java
+++ students/net1814080903342/SearchIdActivity.java
@@ -1,14 +1,35 @@
 package edu.hzuapps.androidlabs;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SearchIdActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_id);
+
+
+
+    final Activity thisActivity = this;
+
+        Button btn= (Button) findViewById(R.id.button3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity,SearchTradingActivity.class);
+                thisActivity.startActivity(intent);
+            }
+        });
+
     }
-}
+
+    }
