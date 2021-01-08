bab9928ab7f005bbe231df767d3b8098de78d109
zhengtingting1 <1871464353@qq.com>
2021-01-08 21:15:15 +0800

    #4 #565 第4次实验

--- students/net1814080903241/app/src/main/java/edu/hzuapps/androidlabs/net181408903241/SerachActivity.java
+++ students/net1814080903241/app/src/main/java/edu/hzuapps/androidlabs/net181408903241/SerachActivity.java
@@ -1,9 +1,13 @@
 package edu.hzuapps.androidlabs.net181408903241;
         import android.os.Bundle;
+        import android.widget.TextView;
+
         import androidx.appcompat.app.AppCompatActivity;
 public class SerachActivity extends  AppCompatActivity {
+    private TextView mse_1;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchword);
+        mse_1 = (TextView) findViewById(R.id.se_1);
     }
 }
