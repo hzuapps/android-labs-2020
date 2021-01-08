6b161ec391349a52c5808b77c695bc6413c72c03
1CHONG <1401115271@qq.com>
2021-01-08 19:33:21 +0800

    #7 #154 第7次实验

--- students/net1814080903115/src/main/java/edu/hzuapps/androidlabs/net1814080903115/PlayActivity.java
+++ students/net1814080903115/src/main/java/edu/hzuapps/androidlabs/net1814080903115/PlayActivity.java
@@ -1,29 +1,117 @@
 package edu.hzuapps.androidlabs.net1814080903115;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class PlayActivity extends AppCompatActivity {
+import java.io.File;
 
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener{
+    private MediaPlayer mediaPlayer = new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        Button btnBack = (Button) findViewById(R.id.button_back);
-        View view = findViewById(R.id.button_back);
 
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(PlayActivity.this, Net1814080903115Activity.class);
-                PlayActivity.this.startActivity(intent);
-                //thisActivity.startActivityForResult(intent, 2016);
-            }
-        });
+        Button btnPlay = (Button) findViewById(R.id.btnPlay);
+        Button btnPause = (Button) findViewById(R.id.btnPause);
+        Button btnStop = (Button) findViewById(R.id.btnStop);
+        btnPlay.setOnClickListener(this);
+        btnPause.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+
+        //权限判断，如果没有权限就请求权限
+        if (ContextCompat.checkSelfPermission(PlayActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PlayActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            initMediaPlayer();//初始化播放器 MediaPlayer
+        }
+
+    }
+
+    private void initMediaPlayer() {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "music.mp3");
+            mediaPlayer.setDataSource(file.getPath());//指定音频文件路径
+            mediaPlayer.setLooping(true);//设置为循环播放
+            mediaPlayer.prepare();//初始化播放器MediaPlayer
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    initMediaPlayer();
+                }else{
+                    Toast.makeText(this, "拒绝权限，将无法使用程序。", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnPlay:
+
+                //如果没在播放中，立刻开始播放。
+                if(!mediaPlayer.isPlaying()){
+
+                    openRawMusicS();
+                }
+                break;
+            case R.id.btnPause:
+                //如果在播放中，立刻暂停。
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                }
+                break;
+            case R.id.btnStop:
+                //如果在播放中，立刻停止。
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.reset();
+                    initMediaPlayer();//初始化播放器 MediaPlayer
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    /**
+     * 打开raw目录下的音乐mp3文件
+     */
+    private void openRawMusicS() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        //用prepare方法，会报错误java.lang.IllegalStateExceptio
+        //mediaPlayer1.prepare();
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
     }
 }
