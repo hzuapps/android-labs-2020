a6b8e01708bf050e473c7e3bbe993a4ce7a12e50
JeesionChone <71253787+JeesionChone@users.noreply.github.com>
2021-01-09 01:49:08 +0800

    #5 #53 第五次实验
    第4、5次实验的代码

--- students/net1814080903310/Net1814080903310Activity.java
+++ students/net1814080903310/Net1814080903310Activity.java
@@ -1,23 +1,37 @@
 package edu.hzuapps.androidlabs.students.net1814080903310;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+import edu.hzuapps.androidlabs.R;
 
 public class Net1814080903310Activity extends AppCompatActivity {
-
+    Button button = null;
+    Button button1 =null;
     @Override
-    protected void onCreat(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.see);
-        final Net1814080903310Activity _this = this;
+
+        button = (Button)findViewById(R.id.video);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(_this, Main1814080903310Activity.class);
-                _this.startActivity(intent);
+                Intent intent =new Intent();
+                intent.setClass(Net1814080903310Activity.this,PlayVideo18148080903310Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        button1 = (Button)findViewById(R.id.info);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Net1814080903310Activity.this,Information1814080903310Activity.class);
+                startActivity(intent);
             }
         });
     }
