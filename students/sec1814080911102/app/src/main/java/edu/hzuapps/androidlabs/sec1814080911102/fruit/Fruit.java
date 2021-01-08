a62db752b3ee54f91844dd0935c4d336e9a25d28
2587900 <www.1289986617@qq.com>
2021-01-08 14:19:28 +0800

    #5 #39 第5次实验

---
+++ students/sec1814080911102/app/src/main/java/edu/hzuapps/androidlabs/sec1814080911102/fruit/Fruit.java
@@ -0,0 +1,11 @@
+package edu.hzuapps.androidlabs.sec1814080911102.fruit;
+
+public class Fruit {
+    private final String text;
+    public Fruit(String text) {
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+}
