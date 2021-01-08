568082030b17199a0ab1f103f324162cc9b32f75
zcjzzf <2268303279@qq.com>
2021-01-08 22:16:58 +0800

    #4 #407 第五次实验

--- students/net1814080903214/app/src/main/java/edu/hzuapps/androidlabs/ResetActivity.java
+++ students/net1814080903214/app/src/main/java/edu/hzuapps/androidlabs/ResetActivity.java
@@ -1,41 +1,61 @@
-import androidx.appcompat.app.AppCompatActivity;
+package edu.hzuapp.androidlabs.net1814080903141;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class HistoryScoreActivity extends AppCompatActivity {
-
+public class RecordActivity extends AppCompatActivity {
+    private Button btnclear;
+    private Button btncheck;
+    private StringBuilder sb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history_score);
+        setContentView(R.layout.activity_record);
 
-        Button returnToGame = (Button)findViewById(R.id.returnToGame);
-        returnToGame.setOnClickListener(new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View v) {
-                                                Intent intent = new Intent();
-                                                intent.setClass(HistoryScoreActivity.this, GameActivity.class);
-                                                startActivity(intent);
-                                            }
-                                        }
-        );
+        btncheck = findViewById(R.id.btncheak);
+        btnclear=findViewById(R.id.btnclear);
 
-        Button returnToMain = (Button)findViewById(R.id.returnToMain);
-        returnToMain.setOnClickListener(new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View v) {
-                                                Intent intent = new Intent();
-                                                intent.setClass(HistoryScoreActivity.this, Net1814080903106Activity.class);
-                                                startActivity(intent);
-                                            }
-                                        }
-        );
+        final RecordActivity thisActivity = this;
 
+
+        btncheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisActivity.showRecord();
+            }
+        });
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisActivity.clearRecord();
+            }
+        });
     }
-
+    private void showRecord() {
+        sb =new  StringBuilder();
+        Cursor cursor=getContentResolver().query(ContentProviderOfRecords.CONTENT_URI,null,null,null,null);
+        if (cursor.moveToFirst()){
+            do {
+                int gscore=cursor.getInt(cursor.getColumnIndex(ContentProviderOfRecords.SCORE));
+                int gbest=cursor.getInt(cursor.getColumnIndex(ContentProviderOfRecords.BEST));
+                sb.append("Score:"+gscore+"     "+"Best:"+gbest+"\n");
+                ((TextView) findViewById(R.id.trecord)).setText(sb);
+            }while ((cursor.moveToNext()));
+        }
+        cursor.close();
+    }
+    private void clearRecord(){
+        getContentResolver().delete(ContentProviderOfRecords.CONTENT_URI,null,null);
+        Toast.makeText(getBaseContext(), //
+                "删除成功! \n" + sb.toString(), Toast.LENGTH_LONG).show();
+        ((TextView) findViewById(R.id.trecord)).setText("");
+    }
 }
--- students/net1814080903214/app/src/main/java/edu/hzuapps/androidlabs/StartActivity.java
+++ students/net1814080903214/app/src/main/java/edu/hzuapps/androidlabs/StartActivity.java
@@ -1,28 +1,45 @@
-import androidx.appcompat.app.AppCompatActivity;
+package edu.hzuapp.androidlabs.net1814080903141;
 
-import android.content.Intent;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
-
+    private Button btnsave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        btnsave=findViewById(R.id.btnsave);
+        final GameActivity thisActivity=this;
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thisActivity.saveRecord();
+            }
+        });
+    }
+    private void saveRecord() {
+        String score = ((EditText) findViewById(R.id.tscore)).getText().toString();
+        String best = ((EditText) findViewById(R.id.tbest)).getText().toString();
 
-        Button toHistoryScore = (Button)findViewById(R.id.toHistoryScore);
-        toHistoryScore.setOnClickListener(new View.OnClickListener() {
-                                              @Override
-                                              public void onClick(View v) {
-                                                  Intent intent = new Intent();
-                                                  intent.setClass(GameActivity.this, HistoryScoreActivity.class);
-                                                  startActivity(intent);
-                                              }
-                                          }
-        );
+        ContentValues record = new ContentValues();
+        record.put(ContentProviderOfRecords.SCORE,score);
+        record.put(ContentProviderOfRecords.BEST, best);
+        Uri uri = getContentResolver()
+                .insert(ContentProviderOfRecords.CONTENT_URI, record);
+        Toast.makeText(getBaseContext(),
+                "保存成功! \n" + uri.toString(), Toast.LENGTH_LONG).show();
 
-
+        this.showRecordInfo("", "");
+    }
+    private void showRecordInfo(String score, String best) {
+        ((EditText) findViewById(R.id.tscore)).setText(score);
+        ((EditText) findViewById(R.id.tbest)).setText(best);
     }
 }
