ad25e89778a62bcabb8fed9b25cfc5ee34033878
Bser-suer <1245076386@qq.com>
2021-01-10 21:35:20 +0800

    #7 #60 第七次实验

--- students/net1814080903319/ReadActivity.java
+++ students/net1814080903319/ReadActivity.java
@@ -1,14 +1,47 @@
 package edu.hzuapps.androidlabs.net1814080903319;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 public class ReadActivity extends AppCompatActivity {
 
+    private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
+        findViewById(R.id.button_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // 请求拍照
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    // 请求拍照
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    // 获取缩略图
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = new ImageView(this);
+            imageView.setImageBitmap(imageBitmap);
+            linearLayout.addView(imageView);
+        }
     }
 }
