9adf63218ecccb6a4f5bb4b1b8976344c5c99ba8
wolkenthrut7 <wolkenthrut@outlook.com>
2021-01-10 15:16:44 +0800

    实验6

--- students/sec1814080911129/Sec1814080911129Activity.java
+++ students/sec1814080911129/Sec1814080911129Activity.java
@@ -1,5 +1,8 @@
 package edu.hzuapps.androidlabs.sec1814080911129;
 
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +10,17 @@ import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.*;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.content.Context;
+import java.io.*;
+
 
 public class Sec1814080911129Activity extends AppCompatActivity {
 
@@ -25,6 +39,15 @@ public class Sec1814080911129Activity extends AppCompatActivity {
             }
         });
 
+        Button http = (Button)findViewById(R.id.httpB);
+        http.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sec1814080911129Activity.this,HttpClientUIActivity.class);
+                startActivity(intent);
+            }
+        });
+
         Button tran = (Button)findViewById(R.id.tranB);
         tran.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,4 +59,6 @@ public class Sec1814080911129Activity extends AppCompatActivity {
 
 
     }
+
 }
+
