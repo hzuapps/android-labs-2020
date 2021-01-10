ebf84278c41b0aca0afe180682cc9d03aae30be8
Chenweikang2 <chen.weikang@qq.com>
2021-01-10 15:32:31 +0800

    #6 #10 第六次实验

---
+++ students/net1806081301122/MainActivity.java
@@ -0,0 +1,46 @@
+package edu.hzuapps.androidlabs.Net1806081301122activity.androidLabs;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button start;
+    Button about;
+    Button help;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.start);
+        about = (Button) findViewById(R.id.about);
+        help = (Button) findViewById(R.id.help);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+}}
--- students/net1806081301122/SettingActivity.java
+++ students/net1806081301122/SettingActivity.java
@@ -1,26 +1,41 @@
-package edu.hzuapps.androidlabs.net1806081301122;
+package edu.hzuapps.androidlabs.Net1806081301122activity.androidLabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class SettingActivity extends AppCompatActivity {
-
+    Button back;
+    Spinner music;
+    Spinner difficulty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        Button button = findViewById(R.id.setting);
-        final SettingActivity _this = this;
-        button.setOnClickListener(new View.OnClickListener() {
+        back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(_this, SettingActivity.class);
-                _this.startActivity(intent);
+                Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
+        Spinner music = (Spinner) findViewById(R.id.music);
+        ArrayAdapter<CharSequence> adapter;
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.music, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        music.setAdapter(adapter);
+
+        Spinner difficulty = (Spinner) findViewById(R.id.music);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.music, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficulty.setAdapter(adapter);
     }
 }
