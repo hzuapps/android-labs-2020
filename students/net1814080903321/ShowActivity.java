0c769a3cef05323be9e0dcb3a86c6b7bc7f6600e
Classic-Z <951473204@qq.com>
2021-01-11 14:55:10 +0800

    #7 #281 实验七和实验截图

--- students/net1814080903321/ShowActivity.java
+++ students/net1814080903321/ShowActivity.java
@@ -1,15 +1,13 @@
 package edu.hzuapp.androidlabs.net1814080903321;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageButton;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
+import android.widget.Toast;
 
 public class ShowActivity extends AppCompatActivity {
 
@@ -18,51 +16,109 @@ public class ShowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
 
-        FloatingActionButton fab = findViewById(R.id.fab1);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ShowActivity.this, Net1814080903321Activity.class);
-                startActivity(i);
-            }
-        });
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    public void myclick(View v){
+        switch (v.getId ()){
+            case R.id.kaonai:
+                showSingleDialog1();
+                break;
+            case R.id.fan:
+                showSingleDialog2();
+                break;
+            case R.id.hanbao:
+                showSingleDialog3();
+                break;
+            case R.id.roujuan:
+                showSingleDialog4();
+                break;
+        }
+    }
 
-        ImageButton kaonai = findViewById(R.id.kaonai);
-        kaonai.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
-                startActivity(i);
-            }
-        });
-        ImageButton fan = findViewById(R.id.fan);
-        fan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
-                startActivity(i);
-            }
-        });
-        ImageButton hanbao = findViewById(R.id.hanbao);
-        hanbao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
-                startActivity(i);
-            }
-        });
-        ImageButton roujuan = findViewById(R.id.roujuan);
-        roujuan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
-                startActivity(i);
-            }
-        });
+    int ide = 0;  //全局变量
+    private void showSingleDialog1() {
+        final String[] guige = {"无糖","半塘" ,"全糖"};
+        final AlertDialog.Builder dialog = new AlertDialog.Builder (this)
+                .setTitle ("请选择规格:")
+                .setSingleChoiceItems (guige, 0, new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ide = which;
+                    }
+                }).setPositiveButton ("确定", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText (ShowActivity.this,"你选择了："+guige[ide],Toast.LENGTH_LONG).show ();
+                        Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
+                        startActivity(i);
+                    }
+                });
 
+        dialog.show ();
+    }
+
+    private void showSingleDialog2() {
+        final String[] guige = {"大份","中份" ,"小份"};
+        final AlertDialog.Builder dialog = new AlertDialog.Builder (this)
+                .setTitle ("请选择规格:")
+                .setSingleChoiceItems (guige, 0, new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ide = which;
+                    }
+                }).setPositiveButton ("确定", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText (ShowActivity.this,"你选择了："+guige[ide],Toast.LENGTH_LONG).show ();
+                        Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+        dialog.show ();
+    }
+
+    private void showSingleDialog3() {
+        final String[] guige = {"不辣","微辣" ,"香辣"};
+        final AlertDialog.Builder dialog = new AlertDialog.Builder (this)
+                .setTitle ("请选择规格:")
+                .setSingleChoiceItems (guige, 0, new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ide = which;
+                    }
+                }).setPositiveButton ("确定", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText (ShowActivity.this,"你选择了："+guige[ide],Toast.LENGTH_LONG).show ();
+                        Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+        dialog.show ();
+    }
+
+    private void showSingleDialog4() {
+        final String[] guige = {"大份","中份" ,"小份"};
+        final AlertDialog.Builder dialog = new AlertDialog.Builder (this)
+                .setTitle ("请选择规格:")
+                .setSingleChoiceItems (guige, 0, new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ide = which;
+                    }
+                }).setPositiveButton ("确定", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText (ShowActivity.this,"你选择了："+guige[ide],Toast.LENGTH_LONG).show ();
+                        Intent i = new Intent(ShowActivity.this, PersonalDetailsActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+        dialog.show ();
     }
 
 }
