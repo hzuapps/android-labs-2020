6793e33ee1c553acfd8da6cd17da8acdc8590508
yyih <1622390428@qq.com>
2021-01-15 11:58:25 +0800

    删除不必要的文件

--- students/net1814080903134/app/src/main/java/edu/hzuapp/androidlabs/net1814080903134/fragment/BaseFragment.java
+++
@@ -1,8 +0,0 @@
-package com.lzy.verticalslideview.fragment;
-
-import android.support.v4.app.Fragment;
-
-public abstract class BaseFragment extends Fragment {
-
-    public abstract void goTop();
-}
